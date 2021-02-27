e20b35533fbc8099e377e21b178130fb351071f1
GuyNoimark <guy.noimark27@gmail.com>
2021-02-27 16:34:43 +0200

    Switched to DataTable widget

--- lib/Rank.dart
+++ lib/Rank.dart
@@ -5,32 +5,35 @@ class Rank extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.symmetric(
-        horizontal: 40,
-        vertical: 20,
+        horizontal: 20,
+        // vertical: 20,
       ),
-      child: Table(
-        columnWidths: {
-          0: FractionColumnWidth(.2),
-          1: FractionColumnWidth(.3),
-          2: FractionColumnWidth(.3),
-          4: FractionColumnWidth(.3),
-        },
-        defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-        children: [
-          TableRow(children: [
-            Text('Rank'),
-            Text('Team Number'),
-            Text('Shot in target'),
-            Text('climbs'),
+      child: DataTable(
+        columnSpacing: 10,
+        columns: [
+          DataColumn(label: Text('Rank')),
+          DataColumn(label: Text('Team Number')),
+          DataColumn(label: Text('Shots in target')),
+          DataColumn(label: Text('climbs')),
+        ],
+        rows: [
+          DataRow(cells: [
+            DataCell(Text('1')),
+            DataCell(Text('1690')),
+            DataCell(Text('47')),
+            DataCell(Text('5')),
           ]),
-          TableRow(children: [
-            TableCell(child: Text('1')),
-            TableCell(child: Text('1690')),
-            TableCell(child: Text('47')),
-            TableCell(child: Text('5')),
-            // Text('1690'),
-            // Text('47'),
-            // Text('5'),
+          DataRow(cells: [
+            DataCell(Text('2')),
+            DataCell(Text('3339')),
+            DataCell(Text('0')),
+            DataCell(Text('3')),
+          ]),
+          DataRow(cells: [
+            DataCell(Text('3')),
+            DataCell(Text('1574')),
+            DataCell(Text('15')),
+            DataCell(Text('10')),
           ]),
         ],
       ),
