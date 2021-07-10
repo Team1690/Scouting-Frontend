8e8a84715409103284c7d4542965a2b36d129626
Noam Prag <noam.prag0@gmail.com>
2021-07-11 02:30:24 +0300

    Team number validation

--- lib/views/widgets/teams_dropdown.dart
+++ lib/views/widgets/teams_dropdown.dart
@@ -6,65 +6,86 @@ import 'package:scouting_frontend/net/get_teams_api.dart';
 
 class TeamsDropdown extends StatefulWidget {
   TeamsDropdown({
-    Key key,
-    @required this.onChange,
-    @required this.typeAheadController,
+    final Key key,
+    @required final this.onChange,
+    @required final this.typeAheadController,
   }) : super(key: key);
 
-  Function(int) onChange;
-  TextEditingController typeAheadController;
+  final Function(int) onChange;
+  final TextEditingController typeAheadController;
 
   @override
   _TeamsDropdownState createState() => _TeamsDropdownState();
 }
 
 class _TeamsDropdownState extends State<TeamsDropdown> {
-  // final TextEditingController _typeAheadController = TextEditingController();
-  bool _validate = false;
+  bool isValueEmpty = false;
+  bool isValueInList = true;
+  List<Team> _suggestions = [];
 
   @override
   Widget build(BuildContext context) {
     return TypeAheadField(
         textFieldConfiguration: TextFieldConfiguration(
-          onChanged: (value) => setState(() => _validate = value.isEmpty),
+          onChanged: (final String value) => setState(() {
+            isValueEmpty = value.isEmpty;
+
+            isValueInList = _suggestions
+                .map<String>((team) => team.teamNumber.toString())
+                .contains(value);
+
+            if (!isValueEmpty && isValueInList)
+              widget.onChange(int.parse(value));
+          }),
           controller: widget.typeAheadController,
           inputFormatters: <TextInputFormatter>[
             FilteringTextInputFormatter.digitsOnly
           ],
           keyboardType: TextInputType.number,
           decoration: InputDecoration(
-            prefixIcon: Icon(Icons.search),
-            border: OutlineInputBorder(),
+            prefixIcon: const Icon(Icons.search),
+            border: const OutlineInputBorder(),
             hintText: 'Search Team',
-            errorText: _validate ? 'Value can\'t be empty' : null,
+            errorText: isValueEmpty
+                ? 'Value can\'t be empty'
+                : !isValueInList
+                    ? 'Not a valid team'
+                    : null,
           ),
         ),
-        suggestionsCallback: GetTeamsApi.getTeamsSuggestion,
-        itemBuilder: (context, Team suggestion) {
-          final user = suggestion;
-          return ListTile(
-            title: Text(user.teamNumber.toString()),
-          );
+        suggestionsCallback: (value) async {
+          final List<Team> suggestions =
+              await GetTeamsApi.getTeamsSuggestion(value);
+
+          setState(() => this._suggestions = suggestions);
+
+          return suggestions;
         },
+        itemBuilder: (context, Team suggestion) =>
+            ListTile(title: Text(suggestion.teamNumber.toString())),
         noItemsFoundBuilder: (context) => Container(
               height: 100,
               child: Center(
                 child: Text(
-                  'No Users Found.',
+                  'No Teams Found',
                   style: TextStyle(fontSize: 24),
                 ),
               ),
             ),
-        onSuggestionSelected: (Team suggestion) {
-          final user = suggestion;
+        onSuggestionSelected: (final Team suggestion) {
           widget.typeAheadController.text = suggestion.teamNumber.toString();
           widget.onChange(suggestion.teamNumber);
 
           ScaffoldMessenger.of(context)
             ..removeCurrentSnackBar()
             ..showSnackBar(SnackBar(
-              content: Text('Selected user: ${user.teamNumber}'),
+              content: Text('Selected team: ${suggestion.teamNumber}'),
             ));
+
+          setState(() {
+            isValueEmpty = false;
+            isValueInList = true;
+          });
         });
   }
 }
