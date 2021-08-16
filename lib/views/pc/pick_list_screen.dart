c5e722365acf5b7f4c6e5219e5db9a15a44c5c27
Ido Sobol <idosobol005@gmail.com>
2021-08-16 17:09:19 +0300

    started pickList screen

--- lib/views/pc/pick_list_screen.dart
+++ lib/views/pc/pick_list_screen.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:scouting_frontend/views/globals.dart' as globals;
+import 'package:scouting_frontend/views/pc/widgets/card.dart';
 import 'package:scouting_frontend/views/pc/widgets/dashboard_scaffold.dart';
+import 'package:scouting_frontend/views/pc/widgets/pick_list_widget.dart';
 
 class PickListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return DashboardScaffold(body: Container());
+    return DashboardScaffold(
+        body: Container(
+      child: Row(
+        children: [
+          Expanded(
+              child: DashboardCard(
+                  title: 'first pick list',
+                  body:
+                      Expanded(child: PickList(pickList: globals.secondList)))),
+          Expanded(
+              child: DashboardCard(
+                  title: 'second pick list',
+                  body: Expanded(child: PickList(pickList: globals.thirdList))))
+        ],
+      ),
+    ));
   }
 }
