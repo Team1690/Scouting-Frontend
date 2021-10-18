b6b135700db3ca8a804e4e94fba97a2ceaaf0d42
Tal <biggameng1@gmail.com>
2021-10-18 17:47:35 +0300

    Drawer For Mobile

--- lib/views/app.dart
+++ lib/views/app.dart
@@ -1,21 +1,25 @@
 import 'package:scouting_frontend/net/get_teams_api.dart';
 import 'package:scouting_frontend/views/mobile/main_app_bar.dart';
+import 'package:scouting_frontend/views/mobile/drawer.dart';
 import 'package:scouting_frontend/views/mobile/screens/input_view.dart';
 import 'package:scouting_frontend/views/constants.dart';
 
 import 'package:flutter/material.dart';
 import 'package:scouting_frontend/views/pc/team_info_screen.dart';
+import 'package:scouting_frontend/views/pc/widgets/drawer_1.dart';
+import 'package:scouting_frontend/views/mobile/drawer.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(final BuildContext context) {
     return MaterialApp(
       title: 'Orbit Scouting',
-      home: isPC(context)
+      home: /*isPC(context)
           ? TeamInfoScreen(data: GetTeamsApi.randomData())
-          : Scaffold(appBar: MainAppBar(), body: UserInput()),
+          :*/ Scaffold(appBar: MainAppBar(), body: UserInput(), drawer: Drawer1(),),
       theme: darkModeTheme,
       debugShowCheckedModeBanner: false,
     );
+    
   }
 }
---
+++ lib/views/mobile/drawer.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+
+
+class Drawer1 extends StatefulWidget {
+  const Drawer1({ Key key }) : super(key: key);
+  
+  @override
+  _Drawer1State createState() => _Drawer1State();
+}
+
+class _Drawer1State extends State<Drawer1> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children:  [
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Color(0xFF2A2D3E),
+            ),
+            child: Text(
+              'Options',
+              style: TextStyle(
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.0,
+                color: Colors.white,
+              ),
+            ),
+
+          ),
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: const Text(
+              'Match',
+              style: TextStyle(
+                fontSize: 25.0,
+                letterSpacing: 1.0,
+              ),
+            ),
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => Scaffold()));
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.search),
+            title: const Text(
+              'Specific',
+              style: TextStyle(
+                fontSize: 25.0,
+                letterSpacing: 1.0,
+              ),
+            ),
+          onTap: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) => Scaffold()));
+          },
+          ),
+          ListTile(
+            leading: Icon(Icons.error_outline),
+            title: const Text(
+              'Pit',
+              style: TextStyle(
+                fontSize: 25.0,
+                letterSpacing: 1.0,
+              ),
+            ),
+          onTap: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) => Scaffold()));
+          },
+          ),
+        ],
+      ),
+    );
+  }
+}
