73471a01451839aae76d19f0e566be322debd6e7
ido sobol <74209065+IdoSobol821@users.noreply.github.com>
2021-10-28 19:54:08 +0300

    made query to transfer from team number to team id

--- lib/net/send_match_api.dart
+++ lib/net/send_match_api.dart
@@ -1,5 +1,8 @@
+import 'package:graphql/client.dart';
 import 'package:http/http.dart' as http;
 import 'package:scouting_frontend/models/match_model.dart';
+
+import 'hasura_helper.dart';
 
 class SendMatchApi {
   Match match;
@@ -31,4 +34,30 @@ class SendMatchApi {
 
     return response;
   }
+
+  static Future<Map<int, int>> getIDs() async {
+    print("getIds");
+    final client = getClient();
+    String query = """query MyQuery {
+  team {
+		number
+    id
+  }
 }
+""";
+    final QueryResult result =
+        await client.query(QueryOptions(document: gql(query)));
+    if (result.hasException) {
+      print(result.exception.toString());
+    } //TODO: avoid dynamic
+
+    print(Map.fromIterable(result.data['team'],
+        key: (e) => e["number"], value: (e) => e["id"]));
+    ;
+
+    return Map.fromIterable(result.data['team'],
+        key: (e) => e["number"], value: (e) => e["id"]);
+  }
+}
+//Instance of '_Future<Map<int, int>>'
+//yeyy 200
