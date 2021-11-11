acad604649c9276d16003130da54762d3ed8fe67
ido sobol <74209065+IdoSobol821@users.noreply.github.com>
2021-11-11 19:30:22 +0200

    Added search box to compare screen.

--- lib/views/pc/compare_screen.dart
+++ lib/views/pc/compare_screen.dart
@@ -1,11 +1,14 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:graphql/client.dart';
 import 'package:scouting_frontend/models/team_model.dart';
+import 'package:scouting_frontend/net/hasura_helper.dart';
 import 'package:scouting_frontend/views/constants.dart';
 import 'package:scouting_frontend/views/pc/widgets/card.dart';
 import 'package:scouting_frontend/views/pc/widgets/dashboard_line_chart.dart';
 import 'package:scouting_frontend/views/pc/widgets/dashboard_scaffold.dart';
 import 'package:scouting_frontend/views/pc/widgets/radar_chart.dart';
+import 'package:scouting_frontend/views/pc/widgets/teams_search_box.dart';
 
 // ignore: must_be_immutable
 class CompareScreen extends StatefulWidget {
@@ -22,6 +25,29 @@ class _CompareScreenState extends State<CompareScreen> {
   List<Team> compareTeamsList = [];
   // List tables;
 
+  Future<List<LightTeam>> fetchTeams() async {
+    final client = getClient();
+    final String query = """
+query FetchTeams {
+  team {
+    id
+    number
+    name
+  }
+}
+  """;
+
+    final QueryResult result =
+        await client.query(QueryOptions(document: gql(query)));
+    if (result.hasException) {
+      print(result.exception.toString());
+    } //TODO: avoid dynamic
+    return (result.data['team'] as List<dynamic>)
+        .map((e) => LightTeam(e['id'], e['number'], e['name']))
+        .toList();
+    //.entries.map((e) => LightTeam(e['id']);
+  }
+
   void addTeam(team) => compareTeamsList.add(team);
   void removeTeam(index) => compareTeamsList
       .removeWhere((Team entry) => entry.teamNumber == index.value.teamNumber);
@@ -35,12 +61,42 @@ class _CompareScreenState extends State<CompareScreen> {
             Container(
               child: Row(
                 children: [
-                  // Expanded(
-                  //   flex: 1,
-                  //   child: TeamsSearchBox(
-                  //       teams:new List<Team>(),
-                  //       onChange: (Team team) => setState(() => addTeam(team))),
-                  // ),
+                  Expanded(
+                      flex: 1,
+                      child: FutureBuilder(
+                          future: fetchTeams(),
+                          builder: (context, snapshot) {
+                            if (snapshot.hasError) {
+                              return Text('Error has happened in the future! ' +
+                                  snapshot.error.toString());
+                            } else if (!snapshot.hasData) {
+                              return Stack(
+                                  alignment: AlignmentDirectional.center,
+                                  children: [
+                                    TextField(
+                                      keyboardType: TextInputType.number,
+                                      decoration: InputDecoration(
+                                        prefixIcon: const Icon(Icons.search),
+                                        border: const OutlineInputBorder(),
+                                        hintText: 'Search Team',
+                                        enabled: false,
+                                      ),
+                                    ),
+                                    Center(
+                                      child: CircularProgressIndicator(),
+                                    ),
+                                  ]);
+
+                              // const CircularProgressIndicator();
+                            } else {
+                              return TeamsSearchBox(
+                                  teams: snapshot.data as List<LightTeam>,
+                                  onChange: (LightTeam team) => {
+                                        setState(() =>
+                                            chosenTeam = team.number as Team)
+                                      });
+                            }
+                          })),
                   SizedBox(width: defaultPadding),
                   Expanded(
                     flex: 2,
