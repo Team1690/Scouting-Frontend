ed2373422e85933cc340e3c255490ab34b635e57
amitcha06 <amitchayko@gmail.com>
2021-12-10 01:42:49 -0800

    Added digits textBox and another selector widgets

--- lib/views/mobile/screens/pit_view.dart
+++ lib/views/mobile/screens/pit_view.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_advanced_switch/flutter_advanced_switch.dart';
 import 'package:scouting_frontend/views/constants.dart';
 import 'package:scouting_frontend/views/mobile/Selector.dart';
 import 'package:scouting_frontend/views/mobile/section_divider.dart';
+import 'package:scouting_frontend/views/mobile/switcher.dart';
 
 class PitView extends StatelessWidget {
   @override
@@ -50,6 +52,27 @@ class PitView extends StatelessWidget {
                 'Cim',
                 'MiniCim',
                 'other',
+              ]),
+            ),
+            SectionDivider(label: "Drive Motors On Each Side"),
+            Container(
+              padding: const EdgeInsets.fromLTRB(
+                  940, defaultPadding / 4, 940, defaultPadding / 4),
+              child: TextField(
+            decoration: new InputDecoration(),
+            keyboardType: TextInputType.number,
+            inputFormatters: <TextInputFormatter>[
+    FilteringTextInputFormatter.digitsOnly
+              ]),
+            ),
+            SectionDivider(label: "Shifter"),
+            Container(
+              padding: const EdgeInsets.fromLTRB(
+                  570, defaultPadding / 4, 570, defaultPadding / 4),
+              child: Selector([
+                'None',
+                'Purchased',
+                'Regular',
               ]),
             ),
           ],
