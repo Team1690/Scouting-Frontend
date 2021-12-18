624272c45fb2ad44cf5d842c152f8bb6e2286955
itamarsch <itamarschwartz10@gmail.com>
2021-12-18 17:56:11 +0200

    telling user if upload to hasura worked

--- lib/views/mobile/FirebaseSubmitButton.dart
+++ lib/views/mobile/FirebaseSubmitButton.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:file_picker/file_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -10,43 +11,94 @@ import 'package:scouting_frontend/views/app.dart';
 import 'package:scouting_frontend/views/mobile/FilePickerWidget.dart';
 
 class FireBaseSubmitButton extends StatelessWidget {
-  FireBaseSubmitButton({Key key, this.vars, this.mutation, this.onPressed})
+  FireBaseSubmitButton(
+      {Key key, this.vars, this.mutation, this.resetForm, this.result})
       : super(key: key);
   final Map<String, dynamic> vars;
   final String mutation;
-  Function() onPressed;
+  final Function() resetForm;
+  final FilePickerResult Function() result;
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton.icon(
-        onPressed: uploadResult, icon: Icon(Icons.send), label: Text('submit'));
+        onPressed: () => uploadResult(vars['team_id'] as int, this.result()),
+        icon: Icon(Icons.send),
+        label: Text('submit'));
   }
 
-  void uploadResult() {
-    onPressed();
-
-    if (FilePickerWidget.result == null) {
-      App.mesengerKey.currentState.showSnackBar(
-          SnackBar(backgroundColor: Colors.red, content: Text('Pick a File!')));
+  void uploadResult(int teamid, FilePickerResult result) {
+    if (result == null) {
+      App.messengerKey.currentState.clearSnackBars();
+      App.messengerKey.currentState.showSnackBar(SnackBar(
+          backgroundColor: Colors.red,
+          content: Text(
+            'Pick a File!',
+            style: TextStyle(color: Colors.white),
+          )));
       return;
     }
-    Reference ref = FirebaseStorage.instance.ref(
-        '/files/${DateTime.now()}.${FilePickerWidget.result.files.first.extension}');
+    Reference ref = FirebaseStorage.instance
+        .ref('/files/$teamid.${result.files.first.extension}');
+
     UploadTask task = kIsWeb
-        ? ref.putData(FilePickerWidget.result.files.first.bytes)
+        ? ref.putData(result.files.first.bytes)
         : Platform.isAndroid
-            ? ref.putFile(File('${FilePickerWidget.result.files.first.path}'))
+            ? ref.putFile(File(result.files.first.path))
             : null;
-    if (task == null) return;
+
+    if (task == null) {
+      App.messengerKey.currentState.showSnackBar(SnackBar(
+          backgroundColor: Colors.red,
+          content: Text(
+            'An Error Occurred',
+            style: TextStyle(color: Colors.white),
+          )));
+      return;
+    }
+    TaskState lastState;
     task.snapshotEvents.listen((event) async {
+      if (event.state == TaskState.running && event.state != lastState) {
+        App.messengerKey.currentState.showSnackBar(SnackBar(
+            duration: Duration(seconds: 20),
+            backgroundColor: Colors.blue,
+            content: Text(
+              'Uploading',
+              style: TextStyle(color: Colors.white),
+            )));
+        lastState = event.state;
+      }
       if (event.state == TaskState.success) {
         var url = await ref.getDownloadURL();
         vars['url'] = url;
-        FilePickerWidget.result = null;
-        FilePickerWidget.controller.value = false;
+
         final client = getClient();
-        client
+
+        final queryResult = await client
             .mutate(MutationOptions(document: gql(mutation), variables: vars));
+
+        if (queryResult.hasException) {
+          ref.delete();
+          App.messengerKey.currentState.clearSnackBars();
+          App.messengerKey.currentState.showSnackBar(SnackBar(
+              duration: Duration(seconds: 10),
+              backgroundColor: Colors.red,
+              content: Text(
+                'Upload Failed ' +
+                    queryResult.exception.graphqlErrors.first.message,
+                style: TextStyle(color: Colors.white),
+              )));
+        } else {
+          resetForm();
+
+          App.messengerKey.currentState.clearSnackBars();
+          App.messengerKey.currentState.showSnackBar(SnackBar(
+              backgroundColor: Colors.green,
+              content: Text(
+                'Uploaded succsesfully',
+                style: TextStyle(color: Colors.white),
+              )));
+        }
       }
     });
   }
