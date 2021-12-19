aba4a1fae4d7d7e5480316cb9211d3b1c9d99988
itamarsch <itamarschwartz10@gmail.com>
2021-12-19 16:16:37 +0200

    added more functionality to firebase submit button

--- lib/views/mobile/FirebaseSubmitButton.dart
+++ lib/views/mobile/FirebaseSubmitButton.dart
@@ -4,15 +4,14 @@ import 'package:file_picker/file_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:graphql/client.dart';
 import 'package:progress_state_button/iconed_button.dart';
 import 'package:progress_state_button/progress_button.dart';
 import 'package:scouting_frontend/net/hasura_helper.dart';
-import 'package:scouting_frontend/views/app.dart';
-import 'package:scouting_frontend/views/mobile/FilePickerWidget.dart';
 
 class FireBaseSubmitButton extends StatefulWidget {
-  FireBaseSubmitButton(
+  const FireBaseSubmitButton(
       {Key key, this.vars, this.mutation, this.resetForm, this.result})
       : super(key: key);
   final Map<String, dynamic> vars;
@@ -26,6 +25,8 @@ class FireBaseSubmitButton extends StatefulWidget {
 
 class _FireBaseSubmitButtonState extends State<FireBaseSubmitButton> {
   ButtonState _state = ButtonState.idle;
+  String errorMessage = '';
+  String graphqlErrorMessage = '';
 
   @override
   Widget build(BuildContext context) {
@@ -41,7 +42,7 @@ class _FireBaseSubmitButtonState extends State<FireBaseSubmitButton> {
           color: Colors.blue[400],
         ),
         ButtonState.fail: IconedButton(
-          text: "Error",
+          text: errorMessage,
           icon: Icon(Icons.cancel, color: Colors.white),
           color: Colors.red.shade300,
         ),
@@ -55,93 +56,96 @@ class _FireBaseSubmitButtonState extends State<FireBaseSubmitButton> {
         )
       },
       state: _state,
-      onPressed: () =>
-          uploadResult(widget.vars['team_id'] as int, this.widget.result()),
+      onPressed: () {
+        if (_state == ButtonState.loading) return;
+
+        if (_state == ButtonState.fail) {
+          setState(() {
+            _state = ButtonState.idle;
+          });
+
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return Scaffold(
+              appBar: AppBar(
+                title: Text('Error message'),
+              ),
+              body: Center(
+                child: Text(graphqlErrorMessage),
+              ),
+            );
+          }));
+          return;
+        }
+        uploadResult(widget.vars['team_id'] as int, this.widget.result());
+      },
     );
   }
 
   void uploadResult(int teamid, FilePickerResult result) {
     if (teamid == null) {
       setState(() {
+        errorMessage = 'Pick a Team';
         _state = ButtonState.fail;
       });
 
-      App.messengerKey.currentState.clearSnackBars();
-      App.messengerKey.currentState.showSnackBar(SnackBar(
-          backgroundColor: Colors.red,
-          content: Text(
-            'Pick a Team',
-            style: TextStyle(color: Colors.white),
-          )));
       Future.delayed(Duration(seconds: 5),
           () => setState((() => _state = ButtonState.idle)));
       return;
     } else if (result == null) {
       setState(() {
+        errorMessage = 'Pick a File';
         _state = ButtonState.fail;
       });
 
-      App.messengerKey.currentState.clearSnackBars();
-      App.messengerKey.currentState.showSnackBar(SnackBar(
-          backgroundColor: Colors.red,
-          content: Text(
-            'Pick a File!',
-            style: TextStyle(color: Colors.white),
-          )));
       Future.delayed(Duration(seconds: 5),
           () => setState((() => _state = ButtonState.idle)));
+
       return;
     }
+
     Reference ref = FirebaseStorage.instance
         .ref('/files/$teamid.${result.files.first.extension}');
 
-    UploadTask task = kIsWeb
+    UploadTask firebaseTask = kIsWeb
         ? ref.putData(result.files.first.bytes)
         : Platform.isAndroid
             ? ref.putFile(File(result.files.first.path))
             : null;
 
-    if (task == null) {
-      App.messengerKey.currentState.showSnackBar(SnackBar(
-          backgroundColor: Colors.red,
-          content: Text(
-            'An Error Occurred',
-            style: TextStyle(color: Colors.white),
-          )));
-      return;
+    if (firebaseTask == null) {
+      //this error will only happen if you run the app on a platform which is not web or android and i dont think you can do that
+      throw PlatformException(code: '');
     }
-    TaskState lastState;
-    task.snapshotEvents.listen((event) async {
-      if (event.state == TaskState.running && event.state != lastState) {
+
+    bool running = true;
+
+    firebaseTask.snapshotEvents.listen((event) async {
+      if (event.state == TaskState.running && running) {
         setState(() {
           _state = ButtonState.loading;
         });
-        lastState = event.state;
-      }
-      if (event.state == TaskState.success) {
+        running = false;
+      } else if (event.state == TaskState.success) {
         Map<String, dynamic> vars = Map.from(this.widget.vars);
         var url = await ref.getDownloadURL();
         vars['url'] = url;
 
         final client = getClient();
 
-        final queryResult = await client.mutate(
+        final graphqlQueryResult = await client.mutate(
             MutationOptions(document: gql(widget.mutation), variables: vars));
 
-        if (queryResult.hasException) {
+        if (graphqlQueryResult.hasException) {
           ref.delete();
           setState(() {
             _state = ButtonState.fail;
+            errorMessage = 'Error';
           });
-          App.messengerKey.currentState.clearSnackBars();
-          App.messengerKey.currentState.showSnackBar(SnackBar(
-              duration: Duration(seconds: 5),
-              backgroundColor: Colors.red,
-              content: Text(
-                'Upload Failed ' +
-                    queryResult.exception.graphqlErrors.first.message,
-                style: TextStyle(color: Colors.white),
-              )));
+
+          graphqlErrorMessage = graphqlQueryResult.exception.toString();
+
+          Future.delayed(Duration(seconds: 5),
+              () => setState((() => _state = ButtonState.idle)));
         } else {
           widget.resetForm();
           setState(() {
@@ -150,6 +154,14 @@ class _FireBaseSubmitButtonState extends State<FireBaseSubmitButton> {
           Future.delayed(Duration(seconds: 5),
               () => setState((() => _state = ButtonState.idle)));
         }
+      } else if (event.state == TaskState.error) {
+        setState(() {
+          _state = ButtonState.fail;
+          errorMessage = 'error';
+          graphqlErrorMessage = 'Firebase error';
+          Future.delayed(Duration(seconds: 5),
+              () => setState((() => _state = ButtonState.idle)));
+        });
       }
     });
   }
