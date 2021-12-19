f30d83d0a8c0cb8e519c4550c2a4b4f154766364
itamarsch <itamarschwartz10@gmail.com>
2021-12-19 16:18:53 +0200

    make texteditingcontroller a variable so that i can reset the team selection box

--- lib/views/mobile/TeamSelection.dart
+++ lib/views/mobile/TeamSelection.dart
@@ -5,21 +5,18 @@ import 'package:scouting_frontend/net/hasura_helper.dart';
 import 'package:scouting_frontend/views/pc/widgets/teams_search_box.dart';
 
 class TeamSelection extends StatefulWidget {
-  TeamSelection({ Key key, Function(LightTeam) onChange }) : super(key: key){
-    this.onChange = onChange;
-  }
-
-   Function(LightTeam) onChange;
+  TeamSelection({Key key, this.onChange, this.controller}) : super(key: key);
+  @required
+  final TextEditingController controller;
+  final Function(LightTeam) onChange;
   @override
   State<TeamSelection> createState() => _TeamSelectionState();
 }
 
 class _TeamSelectionState extends State<TeamSelection> {
-
-    Future<List<LightTeam>> fetchTeams() async {
+  Future<List<LightTeam>> fetchTeams() async {
     final client = getClient();
-    final String query =
-        """
+    final String query = """
 query FetchTeams {
   team {
     id
@@ -40,50 +37,48 @@ query FetchTeams {
     //.entries.map((e) => LightTeam(e['id']);
   }
 
-
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-              future: fetchTeams(),
-              builder: (context, snapshot) {
-                if (snapshot.hasError) {
-                  return Text('Error has happened in the future! ' +
-                      snapshot.error.toString());
-                } else if (!snapshot.hasData) {
-                  return Stack(
-                      alignment: AlignmentDirectional.center,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
-                          child: TextField(
-                            keyboardType: TextInputType.number,
-                            decoration: InputDecoration(
-                              prefixIcon: const Icon(Icons.search),
-                              border: const OutlineInputBorder(),
-                              hintText: 'Search Team',
-                              enabled: false,
-                            ),
-                          ),
-                        ),
-                        Center(
-                          child: CircularProgressIndicator(),
-                        ),
-                      ]);
+        future: fetchTeams(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Text('Error has happened in the future! ' +
+                snapshot.error.toString());
+          } else if (!snapshot.hasData) {
+            return Stack(alignment: AlignmentDirectional.center, children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    prefixIcon: const Icon(Icons.search),
+                    border: const OutlineInputBorder(),
+                    hintText: 'Search Team',
+                    enabled: false,
+                  ),
+                ),
+              ),
+              Center(
+                child: CircularProgressIndicator(),
+              ),
+            ]);
 
-                  // const CircularProgressIndicator();
-                } else {
-                  return Padding(
-                    padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
-                    child: TeamsSearchBox(
-                      teams: snapshot.data as List<LightTeam>,
-                      onChange: (LightTeam) {
-                        setState(() {
-                          widget.onChange(LightTeam);
-                        });
-                      },
-                    ),
-                  );
-                }
-              });
+            // const CircularProgressIndicator();
+          } else {
+            return Padding(
+              padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
+              child: TeamsSearchBox(
+                typeAheadController: widget.controller,
+                teams: snapshot.data as List<LightTeam>,
+                onChange: (LightTeam) {
+                  setState(() {
+                    widget.onChange(LightTeam);
+                  });
+                },
+              ),
+            );
+          }
+        });
   }
 }
