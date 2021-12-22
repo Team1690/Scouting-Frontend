e92dd652b6a60711a988d2a9badbe52a0d617bb0
itamarsch <itamarschwartz10@gmail.com>
2021-12-22 20:50:51 +0200

    added horizontal scrolling and changed photo to a fixed size

--- lib/views/pc/widgets/scouting_pit.dart
+++ lib/views/pc/widgets/scouting_pit.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:scouting_frontend/views/mobile/PitVars.dart';
@@ -11,28 +13,33 @@ class ScoutingPit extends StatelessWidget {
   Widget build(BuildContext context) {
     String newUrl = data.url.substring(0, data.url.length);
     return SingleChildScrollView(
-      child: Column(
-        children: [
-          Text(
-            """
-          Drive Train Type: ${data.driveTrainType == PitVars.driveTrainInitialValue ? 'Not Answered' : data.driveTrainType}
-          Drive Train Motor: ${data.driveMotorType == PitVars.driveMotorInitialValue ? 'Not Answered' : data.driveMotorType}
-          Drive Motor Amount: ${data.driveMotorAmount}
-          Shifter: ${data.shifter == null ? 'Not Answered' : data.shifter}
-          Gearbox: ${data.gearbox == null ? 'Not Answered' : data.gearbox}
-          Drive Wheel Type: ${data.driveWheelType}
-          DriveTrain Reliability: ${data.driveTrainReliability}
-          Electronics Reliability: ${data.electronicsReliability}
-          Robot Reliability: ${data.robotReliability}
-          Notes: ${data.notes}
-          """,
-            softWrap: false,
-          ),
-          CachedNetworkImage(
-            imageUrl: newUrl,
-            placeholder: (context, url) => CircularProgressIndicator(),
-          )
-        ],
+      scrollDirection: Axis.horizontal,
+      child: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            Text(
+              """
+            Drive Train Type: ${data.driveTrainType == PitVars.driveTrainInitialValue ? 'Not Answered' : data.driveTrainType}
+            Drive Train Motor: ${data.driveMotorType == PitVars.driveMotorInitialValue ? 'Not Answered' : data.driveMotorType}
+            Drive Motor Amount: ${data.driveMotorAmount}
+            Shifter: ${data.shifter == null ? 'Not Answered' : data.shifter}
+            Gearbox: ${data.gearbox == null ? 'Not Answered' : data.gearbox}
+            Drive Wheel Type: ${data.driveWheelType}
+            DriveTrain Reliability: ${data.driveTrainReliability}
+            Electronics Reliability: ${data.electronicsReliability}
+            Robot Reliability: ${data.robotReliability}
+            Notes: ${data.notes}
+            """,
+              softWrap: false,
+            ),
+            CachedNetworkImage(
+              width: 80,
+              imageUrl: newUrl,
+              placeholder: (context, url) => CircularProgressIndicator(),
+            )
+          ],
+        ),
       ),
     );
   }
