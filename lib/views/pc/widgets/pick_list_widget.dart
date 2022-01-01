814f402df51f80a1c2dd3ac7f05cb48f9a3b803f
itamarsch <itamarschwartz10@gmail.com>
2022-01-01 16:33:04 +0200

    PickListTeam class and using List<PickListTeam> instead if Lits<String>

--- lib/views/pc/widgets/pick_list_widget.dart
+++ lib/views/pc/widgets/pick_list_widget.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:scouting_frontend/models/team_model.dart';
 import 'package:scouting_frontend/views/constants.dart';
 import 'package:flutter_advanced_switch/flutter_advanced_switch.dart';
+import 'package:scouting_frontend/views/globals.dart';
+import 'package:scouting_frontend/views/pc/pick_list_screen.dart';
 
 class PickList extends StatefulWidget {
-  const PickList({
-    Key key,
-    @required this.pickList,
-  });
+  const PickList({Key key, @required this.uiList, this.screen, this.onReorder});
 
-  final List<String> pickList;
+  final List<PickListTeam> uiList;
+  final CurrentScreen screen;
+  final Function(List<PickListTeam> list) onReorder;
 
   @override
   _PickListState createState() => _PickListState();
@@ -20,9 +22,20 @@ class _PickListState extends State<PickList> {
       if (newindex > oldindex) {
         newindex -= 1;
       }
-      final String item = widget.pickList.removeAt(oldindex);
-      widget.pickList.insert(newindex, item);
+      final PickListTeam item = widget.uiList.removeAt(oldindex);
+      widget.uiList.insert(newindex, item);
+      for (int i = 0; i < widget.uiList.length; i++) {
+        switch (widget.screen) {
+          case CurrentScreen.FIRST:
+            widget.uiList[i].firstListIndex = i;
+            break;
+          case CurrentScreen.SECOND:
+            widget.uiList[i].secondListIndex = i;
+            break;
+        }
+      }
     });
+    widget.onReorder(widget.uiList);
   }
 
   @override
@@ -30,18 +43,21 @@ class _PickListState extends State<PickList> {
     return Container(
       child: ReorderableListView(
         children: <Widget>[
-          for (final item in widget.pickList)
-            Card(
+          ...widget.uiList.map<Widget>((e) {
+            e.controller.addListener(() {
+              widget.onReorder(widget.uiList);
+            });
+            return Card(
               color: bgColor,
-              key: ValueKey(item),
+              key: ValueKey(e.toString()),
               elevation: 2,
               child: Container(
                 padding: const EdgeInsets.fromLTRB(
                     0, defaultPadding / 4, 0, defaultPadding / 4),
                 child: ListTile(
-                  title: Text(item),
+                  title: Text(e.toString()),
                   leading: AdvancedSwitch(
-                    controller: AdvancedSwitchController(),
+                    controller: e.controller,
                     activeColor: Colors.red,
                     inactiveColor: primaryColor,
                     activeChild: Text('Taken'),
@@ -52,10 +68,35 @@ class _PickListState extends State<PickList> {
                   ),
                 ),
               ),
-            ),
+            );
+          })
         ],
         onReorder: reorderData,
       ),
     );
+  }
+}
+
+class PickListTeam extends LightTeam {
+  PickListTeam(int id, int number, String name, this.firstListIndex,
+      this.secondListIndex, bool available)
+      : super(id, number, name) {
+    if (id <= 0) {
+      throw ArgumentError('Invalid ID');
+    } else if (number < 0) {
+      throw ArgumentError('Invalid Team Number');
+    } else if (name == '') {
+      throw ArgumentError('Invalid Team Name');
+    }
+    this.controller.value = available;
+  }
+
+  int firstListIndex;
+  int secondListIndex;
+  AdvancedSwitchController controller = AdvancedSwitchController();
+
+  @override
+  String toString() {
+    return '${this.name} ${this.number}';
   }
 }
