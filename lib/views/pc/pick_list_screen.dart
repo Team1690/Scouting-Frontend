85c8bc3b575e33324f6f4aa5b5210fc3ecc06b14
itamarsch <itamarschwartz10@gmail.com>
2022-01-01 16:27:46 +0200

    save to db function and buttons for refreshing saving and  switching picklist

--- lib/views/pc/pick_list_screen.dart
+++ lib/views/pc/pick_list_screen.dart
@@ -1,30 +1,140 @@
 import 'package:flutter/material.dart';
-import 'package:scouting_frontend/views/globals.dart' as globals;
+import 'package:graphql/client.dart';
+import 'package:scouting_frontend/net/hasura_helper.dart';
 import 'package:scouting_frontend/views/pc/widgets/card.dart';
 import 'package:scouting_frontend/views/pc/widgets/dashboard_scaffold.dart';
 import 'package:scouting_frontend/views/pc/widgets/pick_list_widget.dart';
+import 'package:scouting_frontend/views/pc/widgets/pick_list_widget_future.dart';
 
 import '../constants.dart';
 
-class PickListScreen extends StatelessWidget {
+class PickListScreen extends StatefulWidget {
+  @override
+  State<PickListScreen> createState() => _PickListScreenState();
+}
+
+class _PickListScreenState extends State<PickListScreen> {
+  List<PickListTeam> teams;
+  CurrentScreen currentScreen = CurrentScreen.FIRST;
   @override
   Widget build(BuildContext context) {
+    String title;
+    switch (currentScreen) {
+      case CurrentScreen.FIRST:
+        title = 'First PickList';
+        break;
+      case CurrentScreen.SECOND:
+        title = 'Second PickList';
+        break;
+    }
     return DashboardScaffold(
         body: Padding(
       padding: EdgeInsets.all(defaultPadding),
-      child: Row(
+      child: Column(
         children: [
           Expanded(
-              child: DashboardCard(
-                  title: 'first pick list',
-                  body: PickList(pickList: globals.secondList))),
-          SizedBox(width: defaultPadding),
-          Expanded(
-              child: DashboardCard(
-                  title: 'second pick list',
-                  body: PickList(pickList: globals.thirdList)))
+            child: Row(
+              children: [
+                Expanded(
+                  child: DashboardCard(
+                      titleWidgets: [
+                        IconButton(
+                            onPressed: () {
+                              setState(() {
+                                switch (currentScreen) {
+                                  case CurrentScreen.FIRST:
+                                    currentScreen = CurrentScreen.SECOND;
+                                    break;
+                                  case CurrentScreen.SECOND:
+                                    currentScreen = CurrentScreen.FIRST;
+                                    break;
+                                }
+                              });
+                            },
+                            icon: Icon(Icons.arrow_upward)),
+                        IconButton(
+                          onPressed: save,
+                          icon: Icon(Icons.save),
+                        ),
+                        IconButton(
+                            onPressed: () => setState(() {}),
+                            icon: Icon(Icons.refresh))
+                      ],
+                      title: title,
+                      body: PickListFuture(
+                        onReorder: (list) => teams = list,
+                        screen: currentScreen,
+                      )),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     ));
   }
+
+  void save() async {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      duration: Duration(seconds: 5),
+      content: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [Text('Saving', style: TextStyle(color: Colors.white))],
+      ),
+      backgroundColor: Colors.blue,
+    ));
+    final client = getClient();
+    List<PickListTeam> teams = List.from(this.teams);
+    final String query = """
+  mutation M(\$objects: [team_insert_input!]!) {
+  insert_team(objects: \$objects, on_conflict: {constraint: team_pkey, update_columns: [taken, first_picklist_index, second_picklist_index]}) {
+    affected_rows
+    returning {
+      id
+    }
+  }
 }
+
+  """;
+
+    Map<String, dynamic> vars = {
+      "objects": teams
+          .map((e) => {
+                "id": e.id,
+                "name": e.name,
+                "number": e.number,
+                "first_picklist_index": e.firstListIndex,
+                "second_picklist_index": e.secondListIndex,
+                "taken": e.controller.value
+              })
+          .toList()
+    };
+
+    QueryResult result = await client
+        .mutate(MutationOptions(document: gql(query), variables: vars));
+    if (result.hasException) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        duration: Duration(seconds: 5),
+        content: Text('Error: ${result.exception}'),
+        backgroundColor: Colors.red,
+      ));
+    } else {
+      ScaffoldMessenger.of(context).clearSnackBars();
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        duration: Duration(seconds: 2),
+        content: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Saved',
+              style: TextStyle(color: Colors.white),
+            )
+          ],
+        ),
+        backgroundColor: Colors.green,
+      ));
+    }
+  }
+}
+
+enum CurrentScreen { FIRST, SECOND }
