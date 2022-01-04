dd227ce8a7d6b86011fd8a0fc9343731aeb99f42
itamarsch <itamarschwartz10@gmail.com>
2022-01-04 09:57:29 +0200

    ClimbHelper fixing gilads comment

--- lib/models/match_model.dart
+++ lib/models/match_model.dart
@@ -1,16 +1,6 @@
+import 'package:graphql/client.dart';
+import 'package:scouting_frontend/net/hasura_helper.dart';
 import 'package:scouting_frontend/views/mobile/hasura_vars.dart';
-
-int climbId(final int i) {
-  switch (i) {
-    case 0:
-      return 4;
-    case 1:
-      return 2;
-    case 2:
-    default:
-      return 1;
-  }
-}
 
 class Match implements HasuraVars {
   int teamNumber;
@@ -40,11 +30,7 @@ class Match implements HasuraVars {
   Map<String, dynamic> toHasuraVars() {
     return {
       "auto_balls": autoUpperGoal,
-      "climb_id": climbStatus == 0
-          ? 4
-          : climbStatus == 1
-              ? 2
-              : 1,
+      "climb_id": ClimbHelper.climbId(climbStatus),
       "number": matchNumber,
       "team_id": teamId,
       "teleop_inner": teleInner,
@@ -53,5 +39,73 @@ class Match implements HasuraVars {
       "defended_by": 0,
       "initiation_line": true,
     };
+  }
+}
+
+extension ClimbHelper on Match {
+  static bool querySuccess = false;
+  static int _succeeded;
+  static int _noAttempt;
+  static int _failed;
+
+  static int get successId {
+    return _succeeded;
+  }
+
+  static int get noAttemptId {
+    return _noAttempt;
+  }
+
+  static int get failedId {
+    return _failed;
+  }
+
+  static int climbId(final int i) {
+    if (!querySuccess) {
+      throw GraphQLError(message: 'climb id query didnt succeed');
+    }
+    switch (i) {
+      case 0:
+        return successId;
+      case 1:
+        return failedId;
+      case 2:
+      default:
+        return noAttemptId;
+    }
+  }
+
+  static Future<void> queryclimbId() async {
+    final client = getClient();
+    final query = """
+    query MyQuery {
+  climb {
+    id
+    name
+  }
+}
+
+    """;
+    final result = await client.query(QueryOptions(document: gql(query)));
+    if (result.hasException) {
+      throw result.exception;
+    } else if (result.data == null) {
+      throw GraphQLError(message: 'No climb ids found');
+    } else {
+      (result.data['climb']).forEach((element) {
+        switch (element["name"] as String) {
+          case "failed":
+            _failed = element['id'];
+            break;
+          case "No attempt":
+            _noAttempt = element['id'];
+            break;
+          case "Succeeded":
+            _succeeded = element['id'];
+            break;
+        }
+      });
+      querySuccess = true;
+    }
   }
 }
