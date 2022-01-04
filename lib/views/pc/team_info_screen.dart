0f6e95ac40ab617f4edf15e15d1fcc987af69708
itamarsch <itamarschwartz10@gmail.com>
2022-01-04 23:26:11 +0200

    Using team selection futre instead of copied code

--- lib/views/pc/team_info_screen.dart
+++ lib/views/pc/team_info_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:scouting_frontend/models/team_model.dart';
 import 'package:scouting_frontend/net/hasura_helper.dart';
 import 'package:scouting_frontend/views/constants.dart';
+import 'package:scouting_frontend/views/mobile/team_selection_future.dart';
 import 'package:scouting_frontend/views/pc/widgets/card.dart';
 import 'package:scouting_frontend/views/pc/widgets/dashboard_scaffold.dart';
 import 'package:scouting_frontend/views/pc/widgets/team_info_data.dart';
@@ -21,29 +22,6 @@ class TeamInfoScreen extends StatefulWidget {
 }
 
 class _TeamInfoScreenState extends State<TeamInfoScreen> {
-  Future<List<LightTeam>> fetchTeams() async {
-    final client = getClient();
-    final String query = """
-query FetchTeams {
-  team {
-    id
-    number
-    name
-  }
-}
-  """;
-
-    final QueryResult result =
-        await client.query(QueryOptions(document: gql(query)));
-    if (result.hasException) {
-      print(result.exception.toString());
-    } //TODO: avoid dynamic
-    return (result.data['team'] as List<dynamic>)
-        .map((e) => LightTeam(e['id'], e['number'], e['name']))
-        .toList();
-    //.entries.map((e) => LightTeam(e['id']);
-  }
-
   @override
   Widget build(BuildContext context) {
     // print(data[0].msg[0]);
@@ -55,39 +33,11 @@ query FetchTeams {
                 children: [
                   Expanded(
                       flex: 1,
-                      child: FutureBuilder(
-                          future: fetchTeams(),
-                          builder: (context, snapshot) {
-                            if (snapshot.hasError) {
-                              return Text('Error has happened in the future! ' +
-                                  snapshot.error.toString());
-                            } else if (!snapshot.hasData) {
-                              return Stack(
-                                  alignment: AlignmentDirectional.center,
-                                  children: [
-                                    TextField(
-                                      keyboardType: TextInputType.number,
-                                      decoration: InputDecoration(
-                                        prefixIcon: const Icon(Icons.search),
-                                        border: const OutlineInputBorder(),
-                                        hintText: 'Search Team',
-                                        enabled: false,
-                                      ),
-                                    ),
-                                    Center(
-                                      child: CircularProgressIndicator(),
-                                    ),
-                                  ]);
-
-                              // const CircularProgressIndicator();
-                            } else {
-                              return TeamsSearchBox(
-                                  typeAheadController: widget.controller,
-                                  teams: snapshot.data as List<LightTeam>,
-                                  onChange: (LightTeam team) =>
-                                      setState(() => widget.chosenTeam = team));
-                            }
-                          })),
+                      child: TeamSelectionFuture(
+                        controller: widget.controller,
+                        onChange: (newTeam) =>
+                            setState(() => widget.chosenTeam = newTeam),
+                      )),
                   SizedBox(width: defaultPadding),
                   Expanded(
                       flex: 2,
