6013cfe230e04600a0d1351d6a803f7bd6a87e4e
itamarsch <itamarschwartz10@gmail.com>
2022-01-13 07:25:04 +0200

    scouting piy fixed for new season and we dont need to change the url

--- lib/views/pc/widgets/scouting_pit.dart
+++ lib/views/pc/widgets/scouting_pit.dart
@@ -1,41 +1,43 @@
 import "package:flutter/material.dart";
 import "package:scouting_frontend/views/mobile/pit_vars.dart";
+import 'package:scouting_frontend/views/pc/widgets/team_info_data.dart';
 import 'package:scouting_frontend/views/pc/widgets/team_info_data2.dart';
 import "package:cached_network_image/cached_network_image.dart";
 
 class ScoutingPit extends StatelessWidget {
   ScoutingPit(this.data);
-  final PitViewData data;
+  final PitData? data;
   @override
   Widget build(final BuildContext context) {
-    final String newUrl = data.url.substring(0, data.url.length);
-    return SingleChildScrollView(
-      scrollDirection: Axis.vertical,
-      child: Column(
-        children: <Widget>[
-          Text(
-            """
-          Drive Train Type: ${data.driveTrainType == PitVars.driveTrainInitialValue ? 'Not Answered' : data.driveTrainType}
-          Drive Train Motor: ${data.driveMotorType == PitVars.driveMotorInitialValue ? 'Not Answered' : data.driveMotorType}
-          Drive Motor Amount: ${data.driveMotorAmount}
-          Shifter: ${data.shifter}
-          Gearbox: ${data.gearbox}
-          Drive Wheel Type: ${data.driveWheelType}
-          DriveTrain Reliability: ${data.driveTrainReliability}
-          Electronics Reliability: ${data.electronicsReliability}
-          Robot Reliability: ${data.robotReliability}
-          Notes: ${data.notes}
+    return data == null
+        ? Text("No data yet :(")
+        : SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              children: <Widget>[
+                Text(
+                  """
+          Drive Train Type: ${data!.driveTrainType == PitVars.driveTrainInitialValue ? 'Not Answered' : data!.driveTrainType}
+          Drive Train Motor: ${data!.driveMotorType == PitVars.driveMotorInitialValue ? 'Not Answered' : data!.driveMotorType}
+          Drive Motor Amount: ${data!.driveMotorAmount}
+          Shifter: ${data!.shifter}
+          Gearbox: ${data!.gearbox}
+          Drive Wheel Type: ${data!.driveWheelType}
+          DriveTrain Reliability: ${data!.driveTrainReliability}
+          Electronics Reliability: ${data!.electronicsReliability}
+          Robot Reliability: ${data!.robotReliability}
+          Notes: ${data!.notes}
           """,
-            softWrap: false,
-          ),
-          CachedNetworkImage(
-            width: 120,
-            imageUrl: newUrl,
-            placeholder: (final BuildContext context, final String url) =>
-                Center(child: CircularProgressIndicator()),
-          )
-        ],
-      ),
-    );
+                  softWrap: false,
+                ),
+                CachedNetworkImage(
+                  width: 120,
+                  imageUrl: data!.url,
+                  placeholder: (final BuildContext context, final String url) =>
+                      Center(child: CircularProgressIndicator()),
+                )
+              ],
+            ),
+          );
   }
 }
