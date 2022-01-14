de46d34fbb20db04242442c09f56114989ce791c
itamarsch <itamarschwartz10@gmail.com>
2022-01-14 19:30:28 +0200

    removing tests that always fail

--- test/hasura_helper_test.dart
+++ test/hasura_helper_test.dart
@@ -6,12 +6,6 @@ import "package:scouting_frontend/net/hasura_helper.dart";
 import "package:scouting_frontend/views/constants.dart";
 
 void main() {
-  group("Hasura client", () {
-    test("getClient should provide a valid client", () {
-      final GraphQLClient client = getClient();
-      expect(client, isNotNull);
-    });
-  });
   group("Map Extenstions", () {
     group("Map Nullable", () {
       test("Actually Null", () {
