4b88b62de57b05d454ae7bea69821c10cfa6bf44
itamarsch <itamarschwartz10@gmail.com>
2022-01-17 15:26:34 +0200

    dotenv has a test

--- test/hasura_helper_test.dart
+++ test/hasura_helper_test.dart
@@ -1,9 +1,18 @@
+import "dart:io";
+
+import "package:flutter_dotenv/flutter_dotenv.dart";
 import "package:flutter_test/flutter_test.dart";
 
 import "package:scouting_frontend/net/hasura_helper.dart";
 import "package:scouting_frontend/views/constants.dart";
 
 void main() {
+  dotenv.testLoad(fileInput: File("dev.env").readAsStringSync());
+  group("Haura Helper", () {
+    test("Nullabity", () {
+      expect(getClient(), isNotNull);
+    });
+  });
   group("Map Extenstions", () {
     group("Map Nullable", () {
       test("Actually Null", () {
