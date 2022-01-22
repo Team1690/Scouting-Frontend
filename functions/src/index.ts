c0367715b6fb0f9eac26a39c60b51543698e9d3f
Gilad Woloch <25708271+giladwo@users.noreply.github.com>
2022-01-22 04:02:21 +0200

    Add trivial hasura example

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,3 +7,15 @@ export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
+
+export const hasuraExample = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hasura example logs:", {structuredData: true});
+  functions.logger.info(request);
+  response.send({
+    "X-Hasura-User-Id": request.headers.userId,
+    "X-Hasura-Role": "scouter",
+    "X-Hasura-Is-Owner": "false",
+    "X-Hasura-Custom": "not used",
+    "Cache-Control": "max-age=600",
+  });
+});
