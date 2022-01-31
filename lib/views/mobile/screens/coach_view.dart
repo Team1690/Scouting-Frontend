66feaee6cb654bc3a420286f82655eb173a95bb7
itamarsch <itamarschwartz10@gmail.com>
2022-01-31 12:26:39 +0200

    coach matches

--- lib/views/mobile/screens/coach_view.dart
+++ lib/views/mobile/screens/coach_view.dart
@@ -1,3 +1,4 @@
+import "package:carousel_slider/carousel_slider.dart";
 import "package:flutter/material.dart";
 import "package:graphql/client.dart";
 import "package:scouting_frontend/models/map_nullable.dart";
@@ -20,11 +21,11 @@ class _CoachViewState extends State<CoachView> {
         centerTitle: true,
         title: Text("Coach"),
       ),
-      body: FutureBuilder<CoachData>(
-        future: fetchMatch(),
+      body: FutureBuilder<List<CoachData>>(
+        future: fetchMatches(),
         builder: (
           final BuildContext context,
-          final AsyncSnapshot<CoachData> snapshot,
+          final AsyncSnapshot<List<CoachData>> snapshot,
         ) {
           if (snapshot.hasError) {
             return Text(snapshot.error.toString());
@@ -33,63 +34,39 @@ class _CoachViewState extends State<CoachView> {
               child: CircularProgressIndicator(),
             );
           }
-          return snapshot.data.mapNullable(
-                (final CoachData data) => Stack(
-                  children: <Widget>[
-                    Row(
-                      children: <Widget>[
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Container(
-                              child: Column(
-                                children: List<Widget>.generate(
-                                  3,
-                                  (final int index) => Expanded(
-                                    flex: 2,
-                                    child: teamData(
-                                      data.blueAlliance[index],
-                                      context,
-                                    ),
-                                  ),
-                                )..insert(0, Spacer()),
-                              ),
-                              color: Colors.blue,
-                            ),
-                          ),
+          return snapshot.data.mapNullable((final List<CoachData> data) {
+                final List<CoachData> matches = matchTypes
+                    .map(
+                      (final String matchType) => data
+                          .where(
+                            (final CoachData element) =>
+                                element.matchType == matchType,
+                          )
+                          .toList()
+                        ..sort(
+                          (final CoachData a, final CoachData b) =>
+                              a.matchNumber.compareTo(b.matchNumber),
                         ),
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Container(
-                              child: Column(
-                                children: List<Widget>.generate(
-                                  3,
-                                  (final int index) => Expanded(
-                                    flex: 2,
-                                    child: teamData(
-                                      data.redAlliance[index],
-                                      context,
-                                    ),
-                                  ),
-                                )..insert(0, Spacer()),
-                              ),
-                              color: Colors.red,
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                    Align(
-                      alignment: Alignment.topLeft,
-                      child: Padding(
-                        padding: const EdgeInsets.all(20),
-                        child: Text("${data.matchType}: ${data.matchNumber}"),
-                      ),
                     )
-                  ],
-                ),
-              ) ??
+                    .expand((final List<CoachData> element) => element)
+                    .toList();
+                final int initialIndex = matches.indexWhere(
+                  (final CoachData element) => !element.happened,
+                );
+                return CarouselSlider(
+                  options: CarouselOptions(
+                    enableInfiniteScroll: false,
+                    height: double.infinity,
+                    aspectRatio: 2.0,
+                    viewportFraction: 1,
+                    initialPage:
+                        initialIndex == -1 ? matches.length - 1 : initialIndex,
+                  ),
+                  items: matches
+                      .map((final CoachData e) => matchScreen(context, e))
+                      .toList(),
+                );
+              }) ??
               (throw Exception("No data"));
         },
       ),
@@ -97,11 +74,77 @@ class _CoachViewState extends State<CoachView> {
   }
 }
 
+const List<String> matchTypes = <String>[
+  "Quals",
+  "Quarter finals",
+  "Semi finals",
+  "Finals"
+];
+
+Widget matchScreen(final BuildContext context, final CoachData data) => Stack(
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  child: Column(
+                    children: List<Widget>.generate(
+                      3,
+                      (final int index) => Expanded(
+                        flex: 2,
+                        child: teamData(
+                          data.blueAlliance[index],
+                          context,
+                        ),
+                      ),
+                    )..insert(0, Spacer()),
+                  ),
+                  color: Colors.blue,
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  child: Column(
+                    children: List<Widget>.generate(
+                      3,
+                      (final int index) => Expanded(
+                        flex: 2,
+                        child: teamData(
+                          data.redAlliance[index],
+                          context,
+                        ),
+                      ),
+                    )..insert(0, Spacer()),
+                  ),
+                  color: Colors.red,
+                ),
+              ),
+            )
+          ],
+        ),
+        Align(
+          alignment: Alignment.topLeft,
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Text("${data.matchType}: ${data.matchNumber}"),
+          ),
+        )
+      ],
+    );
+
 const String query = """
 query MyQuery {
-  orbit_matches(order_by: {match_number: asc}, where: {happend: {_eq: false}}) {
+  orbit_matches{
+    happened
     match_number
-    match_type
+    match_type{
+      title
+    }
     blue_0_team {
       id
       name
@@ -239,77 +282,82 @@ query MyQuery {
 
 
 """;
-Future<CoachData> fetchMatch() async {
+Future<List<CoachData>> fetchMatches() async {
   final GraphQLClient client = getClient();
   final QueryResult result =
       await client.query(QueryOptions(document: gql(query)));
   return result.mapQueryResult(
     (final Map<String, dynamic>? data) =>
         data.mapNullable((final Map<String, dynamic> data) {
-          final dynamic match = (data["orbit_matches"] as List<dynamic>).first;
-          final int number = match["match_number"] as int;
-          final String matchType = match["match_type"] as String;
-          final List<CoachViewLightTeam> teams =
-              teamValues.map<CoachViewLightTeam>((final String e) {
-            final LightTeam team = LightTeam(
-              match[e]["id"] as int,
-              match[e]["number"] as int,
-              match[e]["name"] as String,
-            );
-            final dynamic avg =
-                match[e]["matches_aggregate"]["aggregate"]["avg"];
-            final double autoLower =
-                (avg["auto_lower"] as double?) ?? double.nan;
+          final List<dynamic> matches =
+              (data["orbit_matches"] as List<dynamic>);
+          return matches.map((final dynamic match) {
+            final int number = match["match_number"] as int;
+            final String matchType = match["match_type"]["title"] as String;
+            final bool happened = match["happened"] as bool;
+            final List<CoachViewLightTeam> teams =
+                teamValues.map<CoachViewLightTeam>((final String e) {
+              final LightTeam team = LightTeam(
+                match[e]["id"] as int,
+                match[e]["number"] as int,
+                match[e]["name"] as String,
+              );
+              final dynamic avg =
+                  match[e]["matches_aggregate"]["aggregate"]["avg"];
+              final double autoLower =
+                  (avg["auto_lower"] as double?) ?? double.nan;
 
-            final double autoUpper =
-                (avg["auto_upper"] as double?) ?? double.nan;
-            final double teleLower =
-                (avg["tele_lower"] as double?) ?? double.nan;
-            final double teleUpper =
-                (avg["tele_upper"] as double?) ?? double.nan;
-            final double avgBallPoints =
-                autoLower * 2 + autoUpper * 4 + teleLower + teleUpper * 2;
-            final Iterable<int> climb =
-                (match[e]["matches_aggregate"]["nodes"] as List<dynamic>)
-                    .map<int>((final dynamic e) => e["climb"]["points"] as int);
+              final double autoUpper =
+                  (avg["auto_upper"] as double?) ?? double.nan;
+              final double teleLower =
+                  (avg["tele_lower"] as double?) ?? double.nan;
+              final double teleUpper =
+                  (avg["tele_upper"] as double?) ?? double.nan;
+              final double avgBallPoints =
+                  autoLower * 2 + autoUpper * 4 + teleLower + teleUpper * 2;
+              final Iterable<int> climb = (match[e]["matches_aggregate"]
+                      ["nodes"] as List<dynamic>)
+                  .map<int>((final dynamic e) => e["climb"]["points"] as int);
 
-            final double climbAvg = climb.isEmpty
-                ? double.nan
-                : climb.length == 1
-                    ? climb.first.toDouble()
-                    : climb.reduce(
-                          (final int value, final int element) =>
-                              value + element,
-                        ) /
-                        climb.length;
+              final double climbAvg = climb.isEmpty
+                  ? double.nan
+                  : climb.length == 1
+                      ? climb.first.toDouble()
+                      : climb.reduce(
+                            (final int value, final int element) =>
+                                value + element,
+                          ) /
+                          climb.length;
 
-            final double autoAim =
-                (((avg["auto_upper"] as double?) ?? double.nan) /
-                        (((avg["auto_upper"] as double?) ?? double.nan) +
-                            ((avg["auto_upper_missed"] as double?) ??
-                                double.nan))) *
-                    100;
-            final double teleAim =
-                (((avg["tele_upper"] as double?) ?? double.nan) /
-                        (((avg["tele_upper"] as double?) ?? double.nan) +
-                            ((avg["tele_upper_missed"] as double?) ??
-                                double.nan))) *
-                    100;
-            return CoachViewLightTeam(
-              avgBallPoints: avgBallPoints,
-              team: team,
-              avgClimbPoints: climbAvg,
-              autoBallAim: autoAim,
-              teleopBallAim: teleAim,
+              final double autoAim =
+                  (((avg["auto_upper"] as double?) ?? double.nan) /
+                          (((avg["auto_upper"] as double?) ?? double.nan) +
+                              ((avg["auto_upper_missed"] as double?) ??
+                                  double.nan))) *
+                      100;
+              final double teleAim =
+                  (((avg["tele_upper"] as double?) ?? double.nan) /
+                          (((avg["tele_upper"] as double?) ?? double.nan) +
+                              ((avg["tele_upper_missed"] as double?) ??
+                                  double.nan))) *
+                      100;
+              return CoachViewLightTeam(
+                avgBallPoints: avgBallPoints,
+                team: team,
+                avgClimbPoints: climbAvg,
+                autoBallAim: autoAim,
+                teleopBallAim: teleAim,
+              );
+            }).toList();
+
+            return CoachData(
+              happened: happened,
+              blueAlliance: teams.sublist(0, 3),
+              redAlliance: teams.sublist(3),
+              matchNumber: number,
+              matchType: matchType,
             );
           }).toList();
-
-          return CoachData(
-            blueAlliance: teams.sublist(0, 3),
-            redAlliance: teams.sublist(3),
-            matchNumber: number,
-            matchType: matchType,
-          );
         }) ??
         (throw Exception("No data :(")),
   );
@@ -341,12 +389,14 @@ class CoachViewLightTeam {
 
 class CoachData {
   const CoachData({
+    required this.happened,
     required this.blueAlliance,
     required this.redAlliance,
     required this.matchNumber,
     required this.matchType,
   });
   final int matchNumber;
+  final bool happened;
   final List<CoachViewLightTeam> blueAlliance;
   final List<CoachViewLightTeam> redAlliance;
   final String matchType;
