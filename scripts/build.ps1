e69baceb75991f5feba3b69aabfdf487cb68ef54
itamarsch <itamarschwartz10@gmail.com>
2022-02-02 11:24:21 +0200

    build script

---
+++ scripts/build.ps1
@@ -0,0 +1,19 @@
+
+$Commit =  git show -s --format=%B | Select-Object -First 1
+
+if(!(Test-Path -Path ".\build\web")){
+    mkdir ".\build\web"
+}
+
+Set-Location ".\build\web\"
+
+if (Test-Path -Path "./.git") {
+    git pull origin master
+} else {
+    git clone git@github.com:itamarsch/itamarsch.github.io.git .
+}
+flutter build web --web-renderer html --release 
+git add . 
+git commit -m $Commit
+git push origin master
+ Set-Location ../..
