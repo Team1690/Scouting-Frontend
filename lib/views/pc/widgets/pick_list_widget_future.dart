f9f38d65aae23509ab500ac0a0630ff2378390eb
itamarsch <itamarschwartz10@gmail.com>
2022-02-02 09:06:48 +0200

    query and parse of query

--- lib/views/pc/widgets/pick_list_widget_future.dart
+++ lib/views/pc/widgets/pick_list_widget_future.dart
@@ -47,14 +47,31 @@ class _PickListFutureState extends State<PickListFuture> {
   Future<List<PickListTeam>> fetchTeams() async {
     final GraphQLClient client = getClient();
     final String query = """
-    query MyQuery {
+ query MyQuery {
   team {
-    taken
-    second_picklist_index
     first_picklist_index
     id
     name
     number
+    second_picklist_index
+    taken
+    matches_aggregate {
+      aggregate {
+        avg {
+          auto_lower
+          auto_missed
+          auto_upper
+          tele_lower
+          tele_missed
+          tele_upper
+        }
+      }
+      nodes {
+        climb {
+          points
+        }
+      }
+    }
   }
 }
 
@@ -64,21 +81,64 @@ class _PickListFutureState extends State<PickListFuture> {
 
     final List<PickListTeam> teams = result.mapQueryResult(
       (final Map<String, dynamic>? data) =>
-          data.mapNullable(
-            (final Map<String, dynamic> pickListTeams) =>
-                (pickListTeams["team"] as List<dynamic>)
-                    .map(
-                      (final dynamic e) => PickListTeam(
-                        e["id"] as int,
-                        e["number"] as int,
-                        e["name"] as String,
-                        e["first_picklist_index"] as int,
-                        e["second_picklist_index"] as int,
-                        e["taken"] as bool,
-                      ),
-                    )
-                    .toList(),
-          ) ??
+          data.mapNullable((final Map<String, dynamic> pickListTeams) {
+            return (pickListTeams["team"] as List<dynamic>)
+                .map((final dynamic e) {
+              final dynamic avg = e["matches_aggregate"]["aggregate"]["avg"];
+              final double autoLower =
+                  (avg["auto_lower"] as double?) ?? double.nan;
+
+              final double autoUpper =
+                  (avg["auto_upper"] as double?) ?? double.nan;
+              final double teleLower =
+                  (avg["tele_lower"] as double?) ?? double.nan;
+              final double teleUpper =
+                  (avg["tele_upper"] as double?) ?? double.nan;
+              final double avgBallPoints =
+                  autoLower * 2 + autoUpper * 4 + teleLower + teleUpper * 2;
+              final Iterable<int> climb = (e["matches_aggregate"]["nodes"]
+                      as List<dynamic>)
+                  .map<int>((final dynamic e) => e["climb"]["points"] as int);
+
+              final double climbAvg = climb.isEmpty
+                  ? double.nan
+                  : climb.length == 1
+                      ? climb.first.toDouble()
+                      : climb.reduce(
+                            (final int value, final int element) =>
+                                value + element,
+                          ) /
+                          climb.length;
+
+              final double autoAim =
+                  (((avg["auto_upper"] as double? ?? double.nan) +
+                              (avg["auto_lower"] as double? ?? double.nan)) /
+                          ((avg["auto_upper"] as double? ?? double.nan) +
+                              (avg["auto_missed"] as double? ?? double.nan) +
+                              (avg["auto_lower"] as double? ?? double.nan))) *
+                      100;
+              final double teleAim =
+                  (((avg["tele_upper"] as double? ?? double.nan) +
+                              (avg["tele_lower"] as double? ?? double.nan)) /
+                          ((avg["tele_upper"] as double? ?? double.nan) +
+                              (avg["tele_missed"] as double? ?? double.nan) +
+                              (avg["tele_lower"] as double? ?? double.nan))) *
+                      100;
+
+              return PickListTeam(
+                id: e["id"] as int,
+                number: e["number"] as int,
+                name: e["name"] as String,
+                firstListIndex: e["first_picklist_index"] as int,
+                secondListIndex: e["second_picklist_index"] as int,
+                taken: e["taken"] as bool,
+                autoAim: autoAim,
+                teleAim: teleAim,
+                avgBallPoints: avgBallPoints,
+                avgClimbPoints: climbAvg,
+              );
+            }).toList();
+          }) ??
           <PickListTeam>[],
     );
 
