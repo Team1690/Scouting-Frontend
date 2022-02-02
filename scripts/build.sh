09c5225b79fc6d35cc7a56ba1aa417874dede424
Itamar Schwartz <itamarschwartz10@gmail.com>
2022-02-02 12:20:23 +0200

    build script

---
+++ scripts/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+Commit=$(git log -1 --pretty=%B | head -1)
+
+if [[ ! -d ./build/web ]]
+then
+	mkdir build/web
+    
+fi
+
+cd build/web/
+
+if [[ -d ./.git ]]
+then
+	echo ".git exists"
+	git pull origin master
+else
+	echo ".git doesn't exist"
+	git clone git@github.com:itamarsch/itamarsch.github.io.git .
+fi
+
+
+#flutter build web --web-renderer html --release 
+git add . 
+git commit -m "$Commit"
+git push origin master
+cd ../..
