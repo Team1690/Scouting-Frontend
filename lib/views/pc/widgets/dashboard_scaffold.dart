abb8e83992be9be9df8658d23086e5f572b259dd
itamarsch <itamarschwartz10@gmail.com>
2022-02-05 14:19:10 +0200

    keyboard shortcuts

--- lib/views/pc/widgets/dashboard_scaffold.dart
+++ lib/views/pc/widgets/dashboard_scaffold.dart
@@ -1,4 +1,9 @@
 import "package:flutter/material.dart";
+import "package:flutter/services.dart";
+import "package:scouting_frontend/views/pc/compare_screen.dart";
+import "package:scouting_frontend/views/pc/pick_list_screen.dart";
+import "package:scouting_frontend/views/pc/scatters_screen.dart";
+import "package:scouting_frontend/views/pc/team_info_screen.dart";
 
 import "package:scouting_frontend/views/pc/widgets/navigation_tab.dart";
 
@@ -11,18 +16,79 @@ class DashboardScaffold extends StatelessWidget {
 
   @override
   Widget build(final BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      // appBar: AppBar(title: Text('Orbit Scouting')),
-      body: SafeArea(
-        child: Row(
-          children: <Widget>[
-            Expanded(
-              child: NavigationTab(),
-            ),
-            Expanded(flex: 5, child: body)
-          ],
+    return RawKeyboardListener(
+      autofocus: true,
+      focusNode: FocusNode(),
+      onKey: (final RawKeyEvent event) {
+        keyboardShortcut<TeamInfoScreen>(
+          context,
+          event,
+          TeamInfoScreen(),
+          (final RawKeyEvent event) =>
+              event.isControlPressed &&
+              event.physicalKey == PhysicalKeyboardKey.keyM &&
+              event.runtimeType == RawKeyDownEvent,
+        );
+
+        keyboardShortcut<PickListScreen>(
+          context,
+          event,
+          PickListScreen(),
+          (final RawKeyEvent event) =>
+              event.isControlPressed &&
+              event.physicalKey == PhysicalKeyboardKey.comma &&
+              event.runtimeType == RawKeyDownEvent,
+        );
+
+        keyboardShortcut<CompareScreen<int>>(
+          context,
+          event,
+          CompareScreen<int>(),
+          (final RawKeyEvent event) =>
+              event.isControlPressed &&
+              event.physicalKey == PhysicalKeyboardKey.period &&
+              event.runtimeType == RawKeyDownEvent,
+        );
+
+        keyboardShortcut<ScattersScreen>(
+          context,
+          event,
+          ScattersScreen(),
+          (final RawKeyEvent event) =>
+              event.isControlPressed &&
+              event.physicalKey == PhysicalKeyboardKey.slash &&
+              event.runtimeType == RawKeyDownEvent,
+        );
+      },
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        // appBar: AppBar(title: Text('Orbit Scouting')),
+        body: SafeArea(
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: NavigationTab(),
+              ),
+              Expanded(flex: 5, child: body)
+            ],
+          ),
         ),
+      ),
+    );
+  }
+}
+
+void keyboardShortcut<E extends Widget>(
+  final BuildContext context,
+  final RawKeyEvent event,
+  final E widget,
+  final bool Function(RawKeyEvent) predicate,
+) {
+  if (predicate(event)) {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute<E>(
+        builder: (final BuildContext context) => widget,
       ),
     );
   }
