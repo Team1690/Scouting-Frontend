8ce200ff29be99f9e97bbbee284bca5945da7e74
itamarsch <itamarschwartz10@gmail.com>
2022-02-13 08:26:52 +0200

    adding colors_index to light team

--- lib/models/team_model.dart
+++ lib/models/team_model.dart
@@ -1,6 +1,7 @@
 class LightTeam {
-  LightTeam(this.id, this.number, this.name);
+  LightTeam(this.id, this.number, this.name, this.colorsIndex);
   final int id;
   final int number;
   final String name;
+  final int colorsIndex;
 }
