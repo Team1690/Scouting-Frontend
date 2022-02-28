9a2370d56219183a6eef923a2f10cdf6ca586dc0
itamarsch <itamarschwartz10@gmail.com>
2022-02-28 17:31:26 +0200

    changing notes font size

--- lib/views/mobile/screens/coach_team_info_data.dart
+++ lib/views/mobile/screens/coach_team_info_data.dart
@@ -685,7 +685,10 @@ Widget pitScouting(final PitData data, final BuildContext context) =>
           ),
           Align(
             alignment: Alignment.center,
-            child: Text("Notes"),
+            child: Text(
+              "Notes",
+              style: TextStyle(fontSize: 18),
+            ),
           ),
           Text(
             data.notes,
