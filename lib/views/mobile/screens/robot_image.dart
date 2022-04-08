b528f29cb7a63f148da5d9aaa9e1aebbc7093cac
itamarsch <itamarschwartz10@gmail.com>
2022-04-08 15:53:01 +0300

    Changing if to mapNullable

--- lib/views/mobile/screens/robot_image.dart
+++ lib/views/mobile/screens/robot_image.dart
@@ -2,6 +2,7 @@ import "package:cached_network_image/cached_network_image.dart";
 import "package:flutter/material.dart";
 import "package:graphql/client.dart";
 import "package:scouting_frontend/models/map_nullable.dart";
+
 import "package:scouting_frontend/net/hasura_helper.dart";
 
 class RobotImage extends StatelessWidget {
@@ -68,22 +69,21 @@ class RobotImageButton extends StatelessWidget {
   @override
   Widget build(final BuildContext context) => IconButton(
         onPressed: () {
-          final int? teamId = this.teamId;
-          if (teamId != null) {
-            ScaffoldMessenger.of(context).clearSnackBars();
-            Navigator.of(context).push(
-              MaterialPageRoute<RobotImage>(
-                builder: (final BuildContext context) => RobotImage(teamId),
-              ),
-            );
-          } else {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                backgroundColor: Colors.red,
-                content: Text("Cant show photo of team without team selected"),
-              ),
-            );
-          }
+          teamId.mapNullable((final int teamId) {
+                ScaffoldMessenger.of(context).clearSnackBars();
+                Navigator.of(context).push(
+                  MaterialPageRoute<RobotImage>(
+                    builder: (final BuildContext context) => RobotImage(teamId),
+                  ),
+                );
+              }) ??
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  backgroundColor: Colors.red,
+                  content:
+                      Text("Cant show photo of team without team selected"),
+                ),
+              );
         },
         icon: Icon(Icons.camera_alt),
       );
