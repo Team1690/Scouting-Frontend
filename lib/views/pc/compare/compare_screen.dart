7bc65c882a5a3ee46537b79febd94e970f5b047e
itamarsch <itamarschwartz10@gmail.com>
2022-04-10 21:44:40 +0300

    initial teams in compare

--- lib/views/pc/compare/compare_screen.dart
+++ lib/views/pc/compare/compare_screen.dart
@@ -16,14 +16,16 @@ import "package:scouting_frontend/views/common/dashboard_scaffold.dart";
 import "package:scouting_frontend/models/map_nullable.dart";
 
 class CompareScreen<E extends num> extends StatefulWidget {
+  CompareScreen([this.initialTeams = const <LightTeam>[]]);
+  final List<LightTeam> initialTeams;
   @override
   _CompareScreenState<E> createState() => _CompareScreenState<E>();
 }
 
 class _CompareScreenState<E extends num> extends State<CompareScreen<E>> {
-  final SplayTreeSet<LightTeam> teams = SplayTreeSet<LightTeam>(
+  late final SplayTreeSet<LightTeam> teams = SplayTreeSet<LightTeam>(
     (final LightTeam p0, final LightTeam p1) => p0.id.compareTo(p1.id),
-  );
+  )..addAll(widget.initialTeams);
   final TextEditingController controller = TextEditingController();
   void removeTeam(final MapEntry<int, LightTeam> index) {
     teams.removeWhere(
