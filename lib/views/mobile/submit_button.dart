89c9dd3408cbacd840cf06c2c46a38884714c830
itamarsch <itamarschwartz10@gmail.com>
2022-04-16 01:29:18 +0300

    added error for input validation

--- lib/views/mobile/submit_button.dart
+++ lib/views/mobile/submit_button.dart
@@ -52,9 +52,6 @@ class _SubmitButtonState extends State<SubmitButton> {
         )
       },
       onPressed: () async {
-        if (!widget.validate()) {
-          return;
-        }
         if (_state == ButtonState.fail) {
           Navigator.push(
             context,
@@ -71,6 +68,18 @@ class _SubmitButtonState extends State<SubmitButton> {
               },
             ),
           );
+        }
+        if (!widget.validate()) {
+          setState(() {
+            _state = ButtonState.fail;
+            _errorMessage = "You forgot to enter some fields";
+          });
+          Future<void>.delayed(Duration(seconds: 5), () {
+            setState(() {
+              _state = ButtonState.idle;
+            });
+          });
+          return;
         }
         if (_state == ButtonState.loading) {
           return;
