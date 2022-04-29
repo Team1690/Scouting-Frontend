3ca1cd0d91885c2de275bc24b63d22a3c443045e
itamarsch <itamarschwartz10@gmail.com>
2022-04-29 14:30:28 +0300

    Moved fault tile to new File

--- lib/views/mobile/screens/fault_view.dart
+++ lib/views/mobile/screens/fault_view.dart
@@ -5,9 +5,7 @@ import "package:scouting_frontend/models/team_model.dart";
 import "package:scouting_frontend/net/hasura_helper.dart";
 import "package:scouting_frontend/views/constants.dart";
 import "package:scouting_frontend/views/mobile/screens/fault_view/add_fault.dart";
-import "package:scouting_frontend/views/mobile/screens/fault_view/change_fault_status.dart";
-import "package:scouting_frontend/views/mobile/screens/fault_view/delete_fault.dart";
-import "package:scouting_frontend/views/mobile/screens/fault_view/update_fault_message.dart";
+import "package:scouting_frontend/views/mobile/screens/fault_view/fault_tile.dart";
 import "package:scouting_frontend/views/mobile/side_nav_bar.dart";
 
 class FaultView extends StatelessWidget {
@@ -62,61 +60,6 @@ class FaultView extends StatelessWidget {
           }
         },
       ),
-    );
-  }
-}
-
-class FaultTile extends StatelessWidget {
-  const FaultTile(this.e);
-  final FaultEntry e;
-
-  @override
-  Widget build(final BuildContext context) {
-    return ExpansionTile(
-      trailing: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          EditFault(
-            onFinished: handleQueryResult(context),
-            faultMessage: e.faultMessage,
-            faultId: e.id,
-          ),
-          DeleteFault(
-            faultId: e.id,
-            onFinished: handleQueryResult(context),
-          ),
-          ChangeFaultStatus(
-            faultId: e.id,
-            onFinished: handleQueryResult(context),
-          ),
-        ],
-      ),
-      title: Row(
-        children: <Widget>[
-          Expanded(
-            flex: 3,
-            child: Text(
-              "${e.team.number} ${e.team.name}",
-            ),
-          ),
-          Expanded(
-            flex: 3,
-            child: Text(
-              "Status: ${e.faultStatus}",
-              style: TextStyle(
-                color: faultTitleToColor(
-                  e.faultStatus,
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-      children: <Widget>[
-        ListTile(
-          title: Text(e.faultMessage),
-        )
-      ],
     );
   }
 }
