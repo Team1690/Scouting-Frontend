737fbbd7f9f897238c440deb6ea34e0089d2b0b5
itamarsch <itamarschwartz10@gmail.com>
2022-05-12 19:20:39 +0300

    Changing PageTransisions back to FadeUpwardsPageTransitions after default was changed

--- lib/views/constants.dart
+++ lib/views/constants.dart
@@ -12,6 +12,12 @@ const double defaultPadding = 20.0;
 const BorderRadius defaultBorderRadius = BorderRadius.all(Radius.circular(10));
 
 final ThemeData darkModeTheme = ThemeData.dark().copyWith(
+  pageTransitionsTheme: PageTransitionsTheme(
+    builders: <TargetPlatform, PageTransitionsBuilder>{
+      for (final TargetPlatform platform in TargetPlatform.values)
+        platform: FadeUpwardsPageTransitionsBuilder()
+    },
+  ),
   scaffoldBackgroundColor: bgColor,
   textTheme: GoogleFonts.poppinsTextTheme().apply(bodyColor: Colors.white),
   canvasColor: secondaryColor,
