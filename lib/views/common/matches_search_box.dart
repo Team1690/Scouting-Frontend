8f09014f345e1e0ddc8935fbcbfc327748b0c6f4
itamarsch <itamarschwartz10@gmail.com>
2022-10-06 17:21:40 +0300

    matches searchbox

---
+++ lib/views/common/matches_search_box.dart
@@ -0,0 +1,96 @@
+import "package:flutter/material.dart";
+import "package:flutter_typeahead/flutter_typeahead.dart";
+import "package:scouting_frontend/models/id_providers.dart";
+import "package:scouting_frontend/models/matches_model.dart";
+
+class MatchSearchBox extends StatelessWidget {
+  MatchSearchBox({
+    required this.matches,
+    required this.onChange,
+    required this.typeAheadController,
+  });
+  final List<ScheduleMatch> matches;
+  final void Function(ScheduleMatch) onChange;
+  final TextEditingController typeAheadController;
+  @override
+  Widget build(final BuildContext context) => TypeAheadFormField<ScheduleMatch>(
+        validator: (final String? value) {
+          if (value == "") {
+            return "Please pick a match";
+          }
+          return null;
+        },
+        textFieldConfiguration: TextFieldConfiguration(
+          onSubmitted: (final String number) {
+            try {
+              final ScheduleMatch match = matches.firstWhere(
+                (final ScheduleMatch match) =>
+                    match.matchNumber.toString() == number,
+              );
+              onChange(match);
+              typeAheadController.text =
+                  "${match.matchTypeId} ${match.matchNumber}";
+            } on StateError catch (_) {
+              //ignoed
+            }
+          },
+          onTap: typeAheadController.clear,
+          controller: typeAheadController,
+          keyboardType: TextInputType.text,
+          decoration: InputDecoration(
+            prefixIcon: const Icon(Icons.search),
+            border: const OutlineInputBorder(),
+            hintText: "Search Match",
+          ),
+        ),
+        suggestionsCallback: (final String pattern) => matches
+            .where(
+              (final ScheduleMatch element) =>
+                  element.matchNumber.toString().startsWith(pattern),
+            )
+            .toList()
+          ..sort(
+            (final ScheduleMatch a, final ScheduleMatch b) =>
+                a.matchNumber.compareTo(b.matchNumber),
+          ),
+        itemBuilder:
+            (final BuildContext context, final ScheduleMatch suggestion) =>
+                ListTile(
+          title: Text(
+            "${IdProvider.of(context).matchType.idToName[suggestion.matchTypeId]} ${suggestion.matchNumber}",
+          ),
+        ),
+        transitionBuilder: (
+          final BuildContext context,
+          final Widget suggestionsBox,
+          final AnimationController? controller,
+        ) =>
+            FadeTransition(
+          child: suggestionsBox,
+          opacity: CurvedAnimation(
+            parent: controller!,
+            curve: Curves.fastOutSlowIn,
+          ),
+        ),
+        noItemsFoundBuilder: (final BuildContext context) => Container(
+          height: 60,
+          child: Center(
+            child: Text(
+              "No Matches Found",
+              style: TextStyle(fontSize: 16),
+            ),
+          ),
+        ),
+        onSuggestionSelected: (final ScheduleMatch suggestion) {
+          typeAheadController.text =
+              "${IdProvider.of(context).matchType.idToName[suggestion.matchTypeId]} ${suggestion.matchNumber}";
+
+          onChange(
+            matches[matches.indexWhere(
+              (final ScheduleMatch match) =>
+                  match.matchNumber == suggestion.matchNumber,
+            )],
+          );
+        },
+      );
+}
