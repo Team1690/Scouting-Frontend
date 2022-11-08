d2b76dc5350685a588d02f443aa389741a387ad7
itay naumann <itay.naumann@gmail.com>
2022-11-08 11:12:31 +0200

    adding points counter to each team in specific and finding who are the missing teams in the match

--- lib/views/pc/status/fetch_status.dart
+++ lib/views/pc/status/fetch_status.dart
@@ -1,4 +1,8 @@
+import "package:flutter/cupertino.dart";
 import "package:graphql/client.dart";
+import "package:scouting_frontend/models/id_providers.dart";
+import "package:scouting_frontend/models/matches_model.dart";
+import "package:scouting_frontend/models/matches_provider.dart";
 import "package:scouting_frontend/models/team_model.dart";
 import "package:scouting_frontend/net/hasura_helper.dart";
 import "package:scouting_frontend/views/pc/status/status_screen.dart";
@@ -13,6 +17,7 @@ Stream<List<StatusItem<LightTeam, String>>> fetchPreScoutingStatus(
       specific,
       (final dynamic element) => LightTeam.fromJson(element["team"]),
       (final dynamic e) => e["scouter_name"] as String,
+      (final __, final _) => <String>[],
     );
 
 Stream<List<StatusItem<I, V>>> fetchBase<I, V>(
@@ -20,6 +25,7 @@ Stream<List<StatusItem<I, V>>> fetchBase<I, V>(
   final bool specific,
   final I Function(dynamic) parseI,
   final V Function(dynamic) parseV,
+  final List<V> Function(I identifier, List<V> currentValues) getMissing,
 ) {
   return getClient()
       .subscribe(
@@ -41,6 +47,7 @@ Stream<List<StatusItem<I, V>>> fetchBase<I, V>(
             .entries
             .map<StatusItem<I, V>>(
               (final MapEntry<I, List<V>> e) => StatusItem<I, V>(
+                missingValues: getMissing(e.key, e.value),
                 values: e.value,
                 identifier: e.key,
               ),
@@ -56,6 +63,7 @@ Stream<List<StatusItem<I, V>>> fetchBase<I, V>(
 
 Stream<List<StatusItem<MatchIdentifier, Match>>> fetchStatus(
   final bool specific,
+  final BuildContext context,
 ) =>
     fetchBase(
       getSubscription(specific, false),
@@ -66,14 +74,59 @@ Stream<List<StatusItem<MatchIdentifier, Match>>> fetchStatus(
         isRematch: element["is_rematch"] as bool,
       ),
       (final dynamic e) => Match(
-        team: LightTeam(
-          e["team"]["id"] as int,
-          e["team"]["number"] as int,
-          e["team"]["name"] as String,
-          e["team"]["colors_index"] as int,
+        team: StatusLightTeam(
+          specific
+              ? 0
+              : (e["auto_upper"] as int) * 4 +
+                  (e["auto_lower"] as int) * 2 +
+                  (e["tele_upper"] as int) * 2 +
+                  (e["tele_lower"] as int) +
+                  (e["climb"]["points"] as int),
+          LightTeam(
+            e["team"]["id"] as int,
+            e["team"]["number"] as int,
+            e["team"]["name"] as String,
+            e["team"]["colors_index"] as int,
+          ),
         ),
         scouter: e["scouter_name"] as String,
       ),
+      (final MatchIdentifier identifier, final List<Match> currentValues) {
+        final ScheduleMatch match = MatchesProvider.of(context)
+            .matches
+            .where(
+              (final ScheduleMatch element) =>
+                  element.matchNumber == identifier.number &&
+                  element.matchTypeId ==
+                      IdProvider.of(context)
+                          .matchType
+                          .nameToId[identifier.type],
+            )
+            .toList()
+            .single;
+
+        final List<LightTeam> teams = <LightTeam?>[
+          match.red0,
+          match.red1,
+          match.red2,
+          match.red3,
+          match.blue0,
+          match.blue1,
+          match.blue2,
+          match.blue3
+        ].whereType<LightTeam>().toList();
+        return teams
+            .where(
+              (final LightTeam element) => !currentValues
+                  .map((final Match e) => e.team.team)
+                  .contains(element),
+            )
+            .map(
+              (final LightTeam e) =>
+                  Match(scouter: "?", team: StatusLightTeam(0, e)),
+            )
+            .toList();
+      },
     );
 
 String getSubscription(final bool specific, final bool preScouting) => """
@@ -90,6 +143,17 @@ subscription Status {
     }
     scouter_name
     is_rematch
+    ${!specific ? """
+    auto_upper
+    auto_lower
+    auto_missed
+    tele_upper
+    tele_lower
+    tele_missed
+    climb{
+      points
+    }
+""" : ""}
     match {
       match_number
       match_type {
