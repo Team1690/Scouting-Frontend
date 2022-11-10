30c98c25d537b0407e0b9b434acecccff46e5801
amitcha06 <amitchayko@gmail.com>
2022-11-10 20:13:52 +0200

    added DropdownLine widget for specific input

---
+++ lib/views/mobile/dropdown_line.dart
@@ -0,0 +1,42 @@
+import "package:flutter/material.dart";
+import "package:scouting_frontend/views/mobile/section_divider.dart";
+
+class DropdownLine<T> extends StatelessWidget {
+  DropdownLine({
+    required this.onChange,
+    required this.controller,
+    required this.label,
+    required this.value,
+    required this.onTap,
+  });
+  final String label;
+  final T? value;
+  final void Function() onTap;
+  final void Function(String) onChange;
+  final TextEditingController controller;
+
+  @override
+  Widget build(final BuildContext context) {
+    return Column(
+      children: <Widget>[
+        GestureDetector(
+          onTap: onTap,
+          child: SectionDivider(label: label),
+        ),
+        AnimatedCrossFade(
+          duration: Duration(milliseconds: 300),
+          crossFadeState: value == null
+              ? CrossFadeState.showFirst
+              : CrossFadeState.showSecond,
+          firstChild: Container(),
+          secondChild: TextField(
+            controller: controller,
+            textDirection: TextDirection.rtl,
+            onChanged: onChange,
+            decoration: InputDecoration(hintText: label),
+          ),
+        ),
+      ],
+    );
+  }
+}
