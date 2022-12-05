dcda7ff1d72475b67a22a02e43d95c1fbac9d2c4
itay naumann <itay.naumann@gmail.com>
2022-12-05 16:41:58 +0200

    Switching to Team Selection Future to use a specific case of suggestion builder

--- lib/views/pc/compare/compare_screen.dart
+++ lib/views/pc/compare/compare_screen.dart
@@ -4,7 +4,7 @@ import "package:carousel_slider/carousel_slider.dart";
 import "package:flutter/material.dart";
 import "package:scouting_frontend/models/id_providers.dart";
 import "package:scouting_frontend/models/team_model.dart";
-import "package:scouting_frontend/views/common/teams_search_box.dart";
+import "package:scouting_frontend/views/common/team_selection_future.dart";
 import "package:scouting_frontend/views/constants.dart";
 import "package:scouting_frontend/views/common/card.dart";
 import "package:scouting_frontend/views/mobile/side_nav_bar.dart";
@@ -60,7 +60,7 @@ class _CompareScreenState extends State<CompareScreen> {
                 children: <Widget>[
                   Expanded(
                     flex: isPC(context) ? 1 : 2,
-                    child: TeamsSearchBox(
+                    child: TeamSelectionFuture(
                       teams: TeamProvider.of(context).teams
                         ..removeWhere(teams.contains),
                       onChange: (final LightTeam team) {
@@ -71,7 +71,7 @@ class _CompareScreenState extends State<CompareScreen> {
                           );
                         });
                       },
-                      typeAheadController: controller,
+                      controller: controller,
                     ),
                   ),
                   SizedBox(width: defaultPadding),
