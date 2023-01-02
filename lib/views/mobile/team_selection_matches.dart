04647d4710e0a9cfcb7353673ba9bff24d992339
itay naumann <itay.naumann@gmail.com>
2023-01-03 00:33:52 +0200

    change the code to use the new features

--- lib/views/mobile/team_selection_matches.dart
+++ lib/views/mobile/team_selection_matches.dart
@@ -21,46 +21,16 @@ class TeamSelectionMatches extends StatelessWidget {
     return TeamsSearchBox(
       buildSuggestion: (final LightTeam p0) {
         final ScheduleMatch? match = this.match;
-        return match == null
-            ? "No match in schedule"
-            : match.matchTypeId ==
-                    IdProvider.of(context).matchType.nameToId["Practice"]!
-                ? "${p0.number} ${p0.name}"
-                : p0.id == match.blue0.id
-                    ? "blue1 ${p0.number} ${p0.name}"
-                    : p0.id == match.blue1.id
-                        ? "blue2 ${p0.number} ${p0.name}"
-                        : p0.id == match.blue2.id
-                            ? "blue3 ${p0.number} ${p0.name}"
-                            : p0.id == match.red3?.id
-                                ? "red4 ${p0.number} ${p0.name}"
-                                : p0.id == match.red0.id
-                                    ? "red1 ${p0.number} ${p0.name}"
-                                    : p0.id == match.red1.id
-                                        ? "red2 ${p0.number} ${p0.name}"
-                                        : p0.id == match.red2.id
-                                            ? "red3 ${p0.number} ${p0.name}"
-                                            : p0.id == match.blue3?.id
-                                                ? "blue4 ${p0.number} ${p0.name}"
-                                                : throw Exception(
-                                                    "team not in match",
-                                                  );
+        return match == null ? "team not in match" : match.getTeamStation(p0);
       },
       teams: match == null
           ? <LightTeam>[]
           : match!.matchTypeId ==
                   IdProvider.of(context).matchType.nameToId["Practice"]!
               ? TeamProvider.of(context).teams
-              : <LightTeam?>[
-                  match!.blue0,
-                  match!.blue1,
-                  match!.blue2,
-                  match!.blue3,
-                  match!.red0,
-                  match!.red1,
-                  match!.red2,
-                  match!.red3
-                ].whereType<LightTeam>().toList(),
+              : <LightTeam?>[...match!.blueAlliance, ...match!.redAlliance]
+                  .whereType<LightTeam>()
+                  .toList(),
       typeAheadController: controller,
       dontValidate: dontValidate,
       onChange: onChange,
