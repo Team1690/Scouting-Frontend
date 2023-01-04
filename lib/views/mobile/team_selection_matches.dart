f5105ca2cc3fea6ecaf41a09bf1f4d35ee9901a9
itay naumann <itay.naumann@gmail.com>
2023-01-05 00:22:51 +0200

    Tried to make the getTeamStation here

--- lib/views/mobile/team_selection_matches.dart
+++ lib/views/mobile/team_selection_matches.dart
@@ -21,14 +21,25 @@ class TeamSelectionMatches extends StatelessWidget {
     return TeamsSearchBox(
       buildSuggestion: (final LightTeam p0) {
         final ScheduleMatch? match = this.match;
-        return match == null ? "match not found" : match.getTeamStation(p0);
+        String getTeamStation(final LightTeam team) {
+          int? indexOf(final int allianceInd) {
+            final int? index = match?.alliances[allianceInd].indexOf(team);
+            return index == -1 ? null : index;
+          }
+
+          return indexOf(0) == null
+              ? "${p0.name} blue${indexOf(1)}"
+              : "${p0.name} red${indexOf(0)}";
+        }
+
+        return getTeamStation(p0);
       },
       teams: match == null
           ? <LightTeam>[]
           : match!.matchTypeId ==
                   IdProvider.of(context).matchType.nameToId["Practice"]!
               ? TeamProvider.of(context).teams
-              : <LightTeam?>[...match!.blueAlliance, ...match!.redAlliance]
+              : <LightTeam?>[...match!.alliances[1], ...match!.alliances[0]]
                   .whereType<LightTeam>()
                   .toList(),
       typeAheadController: controller,
