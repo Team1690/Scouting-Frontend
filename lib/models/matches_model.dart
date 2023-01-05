d689d77ffc44053c3535b704ed77097fe6fb8492
itay naumann <itay.naumann@gmail.com>
2023-01-05 23:46:11 +0200

    changed to 2 Lists and removed {color}{place}

--- lib/models/matches_model.dart
+++ lib/models/matches_model.dart
@@ -4,29 +4,15 @@ class ScheduleMatch {
   ScheduleMatch({
     required this.matchNumber,
     required this.matchTypeId,
-    required this.red0,
-    required this.red1,
-    required this.red2,
-    required this.red3,
-    required this.blue0,
-    required this.blue1,
-    required this.blue2,
-    required this.blue3,
     required this.id,
     required this.happened,
-    required this.alliances,
+    required this.redAlliance,
+    required this.blueAlliance,
   });
   final bool happened;
   final int id;
   final int matchNumber;
   final int matchTypeId;
-  final LightTeam red0;
-  final LightTeam red1;
-  final LightTeam red2;
-  final LightTeam? red3;
-  final LightTeam blue0;
-  final LightTeam blue1;
-  final LightTeam blue2;
-  final LightTeam? blue3;
-  final List<List<LightTeam>> alliances;
+  final List<LightTeam> redAlliance;
+  final List<LightTeam> blueAlliance;
 }
