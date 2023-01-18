c6f7d4d12184d72ffeb1bf75a06d16fcbd5e3d93
itay <itaynaumann@gmail.com>
2023-01-18 17:44:49 +0200

    changed to a working query that is more readable and changed the call for the query to match the new way to call the function, this is working while building the app...

--- lib/models/id_helpers.dart
+++ lib/models/id_helpers.dart
@@ -1,37 +1,38 @@
 import "package:graphql/client.dart";
 import "package:scouting_frontend/net/hasura_helper.dart";
 
-String tablesToQuerys(
-  final List<String> tables,
-  final List<String> tablesToOrderByOrderColumn,
-) {
-  String queries = "";
-  for (final String table in tables) {
-    queries += """
-    $table(order_by: {${tablesToOrderByOrderColumn.contains(table) ? "order" : "id"}: asc}) {
-        id
-        title
-    }
-""";
+final String tableFragment = """{
+    id
+    title
   }
-  return queries;
-}
+  """;
+
+String queryEnumTable(final String table) =>
+    """$table(order_by: {id:asc}) $tableFragment""";
+String queryOrderedEnumTable(final String table) =>
+    """$table(order_by: {order: asc}) $tableFragment""";
 
 Future<Map<String, Map<String, int>>> fetchEnums(
-  final List<String> tables,
-  final List<String> tablesToOrderByOrderColumn,
+  final List<String> enums,
+  final List<String> orderedEnums,
 ) async {
+  final List<String> queries = <String>[
+    ...enums.map(queryEnumTable),
+    ...orderedEnums.map(queryOrderedEnumTable),
+  ];
+  enums.addAll(orderedEnums);
   final String query = """
-query FetchEnums{
-   ${tablesToQuerys(tables, tablesToOrderByOrderColumn)}
+query FetchEnums {
+    ${queries.join("\n")}
 }
 """;
+  print(query);
   return (await getClient().query(
     QueryOptions<Map<String, Map<String, int>>>(
       document: gql(query),
       parserFn: (final Map<String, dynamic> result) =>
           <String, Map<String, int>>{
-        for (final String table in tables)
+        for (final String table in enums)
           table: <String, int>{
             for (final dynamic entry in (result[table] as List<dynamic>))
               entry["title"] as String: entry["id"] as int
