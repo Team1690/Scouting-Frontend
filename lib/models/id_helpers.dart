9c92978cb2f0cd00215ea37d51543da23338ede6
itay <itaynaumann@gmail.com>
2023-01-18 22:31:31 +0200

    removed an unnecessery print and avoided modifing enums

--- lib/models/id_helpers.dart
+++ lib/models/id_helpers.dart
@@ -7,8 +7,7 @@ final String tableFragment = """{
   }
   """;
 
-String queryEnumTable(final String table) =>
-    """$table(order_by: {id:asc}) $tableFragment""";
+String queryEnumTable(final String table) => """$table $tableFragment""";
 String queryOrderedEnumTable(final String table) =>
     """$table(order_by: {order: asc}) $tableFragment""";
 
@@ -20,19 +19,17 @@ Future<Map<String, Map<String, int>>> fetchEnums(
     ...enums.map(queryEnumTable),
     ...orderedEnums.map(queryOrderedEnumTable),
   ];
-  enums.addAll(orderedEnums);
   final String query = """
 query FetchEnums {
     ${queries.join("\n")}
 }
 """;
-  print(query);
   return (await getClient().query(
     QueryOptions<Map<String, Map<String, int>>>(
       document: gql(query),
       parserFn: (final Map<String, dynamic> result) =>
           <String, Map<String, int>>{
-        for (final String table in enums)
+        for (final String table in <String>[...enums, ...orderedEnums])
           table: <String, int>{
             for (final dynamic entry in (result[table] as List<dynamic>))
               entry["title"] as String: entry["id"] as int
