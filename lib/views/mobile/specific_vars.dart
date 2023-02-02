ee78dbe7e3d213b01d6f964c8eacba39ecf18c03
itay <itaynaumann@gmail.com>
2023-02-03 01:51:29 +0200

    made appBar have 2 states dependenet on if lightTeam is null,made outer body and match selection easy to access widgets and made it make you choose a team first and only then it shows the whole screen and removed the list of controllers

--- lib/views/mobile/specific_vars.dart
+++ lib/views/mobile/specific_vars.dart
@@ -1,9 +1,6 @@
 import "package:scouting_frontend/models/matches_model.dart";
-import "package:scouting_frontend/models/team_model.dart";
-import "package:scouting_frontend/views/mobile/hasura_vars.dart";
 
-class SpecificVars implements HasuraVars {
-  LightTeam? team;
+class SpecificVars {
   String? drivetrainAndDriving;
   String? intake;
   String? placement;
@@ -14,27 +11,12 @@ class SpecificVars implements HasuraVars {
   bool isRematch = false;
 
   String? faultMessage;
-  @override
-  Map<String, dynamic> toHasuraVars() {
-    return <String, dynamic>{
-      "team_id": team?.id,
-      "drivetrain_and_driving": drivetrainAndDriving,
-      "intake": intake,
-      "placement": placement,
-      "defense": defense,
-      "general_notes": generalNotes,
-      "is_rematch": isRematch,
-      "schedule_match_id": scheduleMatch?.id,
-      "scouter_name": name,
-      if (faultMessage != null) "fault_message": faultMessage
-    };
-  }
 
   void reset() {
     isRematch = false;
     scheduleMatch = null;
     faultMessage = null;
-    team = null;
+
     drivetrainAndDriving = null;
     intake = null;
     placement = null;
