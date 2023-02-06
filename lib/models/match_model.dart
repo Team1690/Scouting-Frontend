4601fc2abf03710dc6d8cf1da0bfc4ceaa2593da
itay <itaynaumann@gmail.com>
2023-02-06 18:09:56 +0200

    removed the use of hasuraVars file in match model and moved the function to the sumbit button and removed the constructor

--- lib/models/match_model.dart
+++ lib/models/match_model.dart
@@ -2,29 +2,10 @@ import "package:flutter/cupertino.dart";
 import "package:scouting_frontend/models/id_providers.dart";
 import "package:scouting_frontend/models/matches_model.dart";
 import "package:scouting_frontend/models/team_model.dart";
-import "package:scouting_frontend/views/mobile/hasura_vars.dart";
 
-class Match implements HasuraVars {
+class Match {
   Match({
-    this.autoConesTop = 0,
-    this.autoConesMid = 0,
-    this.autoConesLow = 0,
-    this.autoConesFailed = 0,
-    this.autoCubesTop = 0,
-    this.autoCubesMid = 0,
-    this.autoCubesLow = 0,
-    this.autoCubesFailed = 0,
-    this.teleConesTop = 0,
-    this.teleConesMid = 0,
-    this.teleConesLow = 0,
-    this.teleConesFailed = 0,
-    this.teleCubesTop = 0,
-    this.teleCubesMid = 0,
-    this.teleCubesLow = 0,
-    this.teleCubesFailed = 0,
-    this.scoutedTeam,
     required this.robotMatchStatusId,
-    this.isRematch = false,
   });
 
   void clear(final BuildContext context) {
@@ -54,56 +35,28 @@ class Match implements HasuraVars {
   }
 
   bool preScouting = false;
-  bool isRematch;
+  bool isRematch = false;
   ScheduleMatch? scheduleMatch;
-  int autoConesTop;
-  int autoConesMid;
-  int autoConesLow;
-  int autoConesFailed;
-  int autoCubesTop;
-  int autoCubesMid;
-  int autoCubesLow;
-  int autoCubesFailed;
-  int teleConesTop;
-  int teleConesMid;
-  int teleConesLow;
-  int teleConesFailed;
-  int teleCubesTop;
-  int teleCubesMid;
-  int teleCubesLow;
-  int teleCubesFailed;
+  int autoConesTop = 0;
+  int autoConesMid = 0;
+  int autoConesLow = 0;
+  int autoConesFailed = 0;
+  int autoCubesTop = 0;
+  int autoCubesMid = 0;
+  int autoCubesLow = 0;
+  int autoCubesFailed = 0;
+  int teleConesTop = 0;
+  int teleConesMid = 0;
+  int teleConesLow = 0;
+  int teleConesFailed = 0;
+  int teleCubesTop = 0;
+  int teleCubesMid = 0;
+  int teleCubesLow = 0;
+  int teleCubesFailed = 0;
   String? name;
   int? autoBalanceStatus;
   int? endgameBalanceStatus;
   int robotMatchStatusId;
 
   LightTeam? scoutedTeam;
-  @override
-  Map<String, dynamic> toHasuraVars() {
-    return <String, dynamic>{
-      "auto_cones_mid": autoConesMid,
-      "auto_cones_top": autoConesTop,
-      "auto_cones_low": autoConesLow,
-      "auto_cones_failed": autoConesFailed,
-      "auto_cubes_mid": autoCubesMid,
-      "auto_cubes_top": autoCubesTop,
-      "auto_cubes_low": autoCubesLow,
-      "auto_cubes_failed": autoCubesFailed,
-      "auto_balance_id": autoBalanceStatus,
-      "endgame_balance_id": endgameBalanceStatus,
-      "team_id": scoutedTeam?.id,
-      "tele_cones_mid": teleConesMid,
-      "tele_cones_top": teleConesTop,
-      "tele_cones_low": teleConesLow,
-      "tele_cones_failed": teleConesFailed,
-      "tele_cubes_mid": teleCubesMid,
-      "tele_cubes_top": teleCubesTop,
-      "tele_cubes_low": teleCubesLow,
-      "tele_cubes_failed": teleCubesFailed,
-      "scouter_name": name,
-      "schedule_match_id": scheduleMatch?.id,
-      "robot_match_status_id": robotMatchStatusId,
-      "is_rematch": isRematch,
-    };
-  }
 }
