95b2a114ca19e44710adb4c0e92a4f821fed6de8
itay <itaynaumann@gmail.com>
2023-02-07 20:16:37 +0200

    changed team info screen to the new way of using teamSelectionFuture and made it get team before building the whole screen

--- lib/views/pc/team_info/team_info_screen.dart
+++ lib/views/pc/team_info/team_info_screen.dart
@@ -6,67 +6,58 @@ import "package:scouting_frontend/views/common/team_selection_future.dart";
 import "package:scouting_frontend/views/common/no_team_selected.dart";
 import "package:scouting_frontend/views/common/dashboard_scaffold.dart";
 import "package:scouting_frontend/views/pc/team_info/team_info_data.dart";
-import "package:scouting_frontend/models/map_nullable.dart";
 
-class TeamInfoScreen extends StatefulWidget {
+class TeamInfoScreen extends StatelessWidget {
   TeamInfoScreen({this.initalTeam});
-  final TextEditingController controller = TextEditingController();
-  final LightTeam? initalTeam;
-  @override
-  State<TeamInfoScreen> createState() => _TeamInfoScreenState(initalTeam);
-}
 
-class _TeamInfoScreenState extends State<TeamInfoScreen> {
-  _TeamInfoScreenState(final LightTeam? team) {
-    team.mapNullable((final LightTeam p0) {
-      chosenTeam = team;
-    });
-  }
-  LightTeam? chosenTeam;
-  @override
-  void initState() {
-    super.initState();
-    chosenTeam.mapNullable((final LightTeam element) {
-      widget.controller.text = "${element.number} ${element.name}";
-    });
-  }
+  final LightTeam? initalTeam;
 
   @override
   Widget build(final BuildContext context) {
     return DashboardScaffold(
       body: Padding(
         padding: const EdgeInsets.all(defaultPadding),
-        child: Column(
+        child: TeamSelectionFuture(
+          initialTeam: initalTeam,
+          teams: TeamProvider.of(context).teams,
+          buildWithoutTeam: (final Widget searchBox, final _) => body(
+            searchBox,
+            NoTeamSelected(),
+          ),
+          buildWithTeam: (
+            final BuildContext context,
+            final LightTeam team,
+            final Widget searchBox,
+            final _,
+          ) =>
+              body(searchBox, TeamInfoData(team)),
+        ),
+      ),
+    );
+  }
+
+  Column body(
+    final Widget searchBox,
+    final Widget infoWidget,
+  ) {
+    return Column(
+      children: <Widget>[
+        Row(
           children: <Widget>[
-            Row(
-              children: <Widget>[
-                Expanded(
-                  flex: 1,
-                  child: TeamSelectionFuture(
-                    teams: TeamProvider.of(context).teams,
-                    controller: widget.controller,
-                    onChange: (final LightTeam newTeam) =>
-                        setState(() => chosenTeam = newTeam),
-                  ),
-                ),
-                SizedBox(width: defaultPadding),
-                Expanded(
-                  flex: 2,
-                  child: Container(),
-                ),
-              ],
-            ),
-            SizedBox(height: defaultPadding),
+            Expanded(flex: 1, child: searchBox),
+            SizedBox(width: defaultPadding),
             Expanded(
-              flex: 10,
-              child: chosenTeam.mapNullable(
-                    TeamInfoData.new,
-                  ) ??
-                  NoTeamSelected(),
+              flex: 2,
+              child: Container(),
             ),
           ],
         ),
-      ),
+        SizedBox(height: defaultPadding),
+        Expanded(
+          flex: 10,
+          child: infoWidget,
+        ),
+      ],
     );
   }
 }
