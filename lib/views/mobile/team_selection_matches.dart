fb1d8412f4b3de86903404ba4a9bd308bdcc238e
itay <itaynaumann@gmail.com>
2023-02-10 12:32:57 +0200

    resolving most of #336 conversation, transferring it's completion to @ItayNaumann :)

--- lib/views/mobile/team_selection_matches.dart
+++ lib/views/mobile/team_selection_matches.dart
@@ -27,11 +27,11 @@ class TeamSelectionMatches extends StatelessWidget {
 
   @override
   Widget build(final BuildContext context) {
-    final bool isNotOfficial = match == null ||
-        match!.matchTypeId ==
-            IdProvider.of(context).matchType.nameToId["Practice"]! ||
-        match!.matchTypeId ==
-            IdProvider.of(context).matchType.nameToId["Pre scouting"]!;
+    final Map<String, int> provider = IdProvider.of(context).matchType.nameToId;
+    final bool isUnofficial = <int>[
+      provider["Practice"]!,
+      provider["Pre scouting"]!
+    ].contains(match.matchTypeId);
     return TeamsSearchBox(
       buildSuggestion: (final LightTeam p0) => match!.getTeamStation(p0)!,
       teams: match == null
