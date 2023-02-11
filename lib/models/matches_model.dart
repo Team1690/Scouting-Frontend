d66997f2b82963b62487ed41ac2ca57edfbb7a27
itay <itaynaumann@gmail.com>
2023-02-12 00:09:44 +0200

    in here we define both types of matches,make their functions and make their parrent class that both types inherit

--- lib/models/matches_model.dart
+++ lib/models/matches_model.dart
@@ -1,4 +1,23 @@
+import "package:collection/collection.dart";
+import "package:flutter/material.dart";
+import "package:scouting_frontend/models/id_providers.dart";
+import "package:scouting_frontend/models/map_nullable.dart";
 import "package:scouting_frontend/models/team_model.dart";
+
+class TeamStation {
+  TeamStation({
+    required this.allianceColor,
+    required this.alliancePos,
+    required this.team,
+  });
+  final LightTeam team;
+  final int alliancePos;
+  final String allianceColor;
+  MaterialColor getColor() =>
+      allianceColor == "blue" ? Colors.blue : Colors.red;
+  String getText() =>
+      "${team.number} ${team.name} - $allianceColor ${alliancePos + 1}";
+}
 
 class ScheduleMatch {
   ScheduleMatch({
@@ -6,28 +25,82 @@ class ScheduleMatch {
     required this.matchTypeId,
     required this.id,
     required this.happened,
-    required this.redAlliance,
-    required this.blueAlliance,
   });
+  List<LightTeam> getTeams(final BuildContext context) =>
+      throw (Exception("tried to reach parent function returnTeams"));
   final bool happened;
   final int id;
   final int matchNumber;
   final int matchTypeId;
-  final List<LightTeam> redAlliance;
-  final List<LightTeam> blueAlliance;
+  String getTeamStationText(final LightTeam team) =>
+      throw (Exception("tried to reach parent function getTeamStation"));
+  List<TeamStation> getStations() => <TeamStation>[];
+  LightTeam? getTeamByStation(final String color, final int index) => null;
+  TeamStation getTeamStation(final LightTeam team) =>
+      throw (Exception("tried to reach parent function getTeamStation"));
+  bool isOfficial() => false;
+}
 
-  String? getTeamStation(final LightTeam team) {
-    String? fieldPositionOf(
-      final List<LightTeam> alliance,
-      final String color,
-    ) {
-      final int index = alliance.indexOf(team);
-      return index == -1
-          ? null
-          : "${team.number} ${team.name} - $color ${index + 1}";
-    }
-
-    return fieldPositionOf(redAlliance, "red") ??
-        fieldPositionOf(blueAlliance, "blue");
+class OfficialMatch extends ScheduleMatch {
+  OfficialMatch({
+    required final bool happened,
+    required final int matchNumber,
+    required final int matchTypeId,
+    required final int id,
+    required this.teams,
+  }) : super(
+          happened: happened,
+          matchNumber: matchNumber,
+          matchTypeId: matchTypeId,
+          id: id,
+        );
+  final List<TeamStation> teams;
+  @override
+  bool isOfficial() => true;
+  @override
+  LightTeam? getTeamByStation(final String color, final int index) {
+    final TeamStation? teamStation = teams.firstWhereOrNull(
+      (final TeamStation station) =>
+          station.allianceColor == color && station.alliancePos == index,
+    );
+    return teamStation
+        .mapNullable((final TeamStation teamStation) => teamStation.team);
   }
+
+  @override
+  TeamStation getTeamStation(final LightTeam team) => teams.firstWhere(
+        (final TeamStation teamStation) => teamStation.team == team,
+        orElse: () => throw (Exception(
+          "${team.number} ${team.name} not found in any alliance",
+        )),
+      );
+  @override
+  List<TeamStation> getStations() => teams;
+  @override
+  List<LightTeam> getTeams(final BuildContext context) =>
+      teams.map((final TeamStation teamStation) => teamStation.team).toList();
+  @override
+  String getTeamStationText(final LightTeam team) =>
+      getTeamStation(team).getText();
+}
+
+class UnofficialMatch extends ScheduleMatch {
+  UnofficialMatch({
+    required final bool happened,
+    required final int matchNumber,
+    required final int matchTypeId,
+    required final int id,
+  }) : super(
+          happened: happened,
+          matchNumber: matchNumber,
+          matchTypeId: matchTypeId,
+          id: id,
+        );
+  @override
+  @override
+  List<LightTeam> getTeams(final BuildContext context) =>
+      TeamProvider.of(context).teams;
+  @override
+  String getTeamStationText(final LightTeam team) =>
+      "${team.number} ${team.name}";
 }
