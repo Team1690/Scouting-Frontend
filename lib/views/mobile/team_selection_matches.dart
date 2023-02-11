dad145c925edff3b1cada291a85db6c6b6304804
itay <itaynaumann@gmail.com>
2023-02-12 01:01:48 +0200

    we change from checking if its official or not to just using the schedule match functions to gain the info we need

--- lib/views/mobile/team_selection_matches.dart
+++ lib/views/mobile/team_selection_matches.dart
@@ -1,5 +1,5 @@
 import "package:flutter/cupertino.dart";
-import "package:scouting_frontend/models/id_providers.dart";
+import "package:scouting_frontend/models/map_nullable.dart";
 import "package:scouting_frontend/models/matches_model.dart";
 import "package:scouting_frontend/models/team_model.dart";
 import "package:scouting_frontend/views/common/teams_search_box.dart";
@@ -26,20 +26,19 @@ class TeamSelectionMatches extends StatelessWidget {
   final Widget Function(Widget, void Function())? buildWithoutTeam;
 
   @override
-  Widget build(final BuildContext context) {
-    return TeamsSearchBox(
-      buildSuggestion: (final LightTeam p0) => match!.getTeamStation(p0)!,
-      teams: match == null
-          ? <LightTeam>[]
-          : match!.matchTypeId ==
-                  IdProvider.of(context).matchType.nameToId["Practice"]!
-              ? TeamProvider.of(context).teams
-              : <LightTeam>[...match!.blueAlliance, ...match!.redAlliance],
-      dontValidate: dontValidate,
-      buildWithTeam: buildWithTeam,
-      onSelected: onSelected,
-      buildWithoutTeam: buildWithoutTeam,
-      initalTeam: initialTeam,
-    );
-  }
+  Widget build(final BuildContext context) => TeamsSearchBox(
+        buildSuggestion: match.mapNullable(
+              (final ScheduleMatch match) => match.getTeamStationText,
+            ) ??
+            (final LightTeam team) => "Please Select A Match",
+        teams: match.mapNullable(
+              (final ScheduleMatch match) => match.getTeams(context),
+            ) ??
+            <LightTeam>[],
+        dontValidate: dontValidate,
+        buildWithTeam: buildWithTeam,
+        onSelected: onSelected,
+        buildWithoutTeam: buildWithoutTeam,
+        initalTeam: initialTeam,
+      );
 }
