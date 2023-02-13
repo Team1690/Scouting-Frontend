2e11d96fb9324bb8468ccd11223b7591746a264e
itay <itaynaumann@gmail.com>
2023-02-13 14:01:35 +0200

    combined teamSelectionMatches and matchSelectionFuture because one's state needs to change the other's as well and moved their definision into the file cause thats the only place we use them

--- lib/views/mobile/team_and_match_selection.dart
+++ lib/views/mobile/team_and_match_selection.dart
@@ -2,6 +2,79 @@ import "package:flutter/material.dart";
 import "package:flutter_typeahead/flutter_typeahead.dart";
 import "package:scouting_frontend/models/id_providers.dart";
 import "package:scouting_frontend/models/matches_model.dart";
+import "package:scouting_frontend/models/matches_provider.dart";
+import "package:scouting_frontend/models/team_model.dart";
+import "package:scouting_frontend/views/common/teams_search_box.dart";
+
+class TeamAndMatchSelection extends StatefulWidget {
+  const TeamAndMatchSelection({
+    required this.onTeamChange,
+    required this.onMatchChange,
+  });
+  final void Function(ScheduleMatch) onMatchChange;
+  final void Function(LightTeam) onTeamChange;
+  @override
+  State<TeamAndMatchSelection> createState() => TeamAndMatchSelectionState();
+}
+
+class TeamAndMatchSelectionState extends State<TeamAndMatchSelection> {
+  final TextEditingController matchController = TextEditingController();
+  final TextEditingController teamNumberController = TextEditingController();
+  ScheduleMatch? scheduleMatch;
+  List<LightTeam> teams = <LightTeam>[];
+  SuggestionsBoxController suggestionsBoxController =
+      SuggestionsBoxController();
+  bool isUnofficial = false;
+
+  @override
+  Widget build(final BuildContext context) {
+    final Map<String, int> provider = IdProvider.of(context).matchType.nameToId;
+    return Column(
+      children: <Widget>[
+        if (MatchesProvider.of(context).matches.isEmpty)
+          Text("No matches found :(")
+        else
+          MatchSearchBox(
+            typeAheadController: matchController,
+            matches: MatchesProvider.of(context).matches,
+            onChange: (final ScheduleMatch selectedMatch) {
+              setState(() {
+                widget.onMatchChange(selectedMatch);
+                scheduleMatch = selectedMatch;
+
+                isUnofficial = <int>[
+                  provider["Practice"]!,
+                  provider["Pre scouting"]!
+                ].contains(selectedMatch.matchTypeId);
+                teams = isUnofficial
+                    ? TeamProvider.of(context).teams
+                    : <LightTeam>[
+                        ...selectedMatch.blueAlliance,
+                        ...selectedMatch.redAlliance
+                      ];
+                suggestionsBoxController.close();
+              });
+            },
+          ),
+        SizedBox(
+          height: 15,
+        ),
+        TeamsSearchBox(
+          buildSuggestion: (final LightTeam currentTeam) => isUnofficial
+              ? "${currentTeam.number} ${currentTeam.name}"
+              : scheduleMatch!.getTeamStation(currentTeam) ?? "",
+          teams: teams,
+          typeAheadController: teamNumberController,
+          onChange: (final LightTeam team) {
+            setState(() {
+              widget.onTeamChange(team);
+            });
+          },
+        ),
+      ],
+    );
+  }
+}
 
 class MatchSearchBox extends StatelessWidget {
   MatchSearchBox({
