7b4752733fe702941e396fa5da2b3877ff025769
itay <itaynaumann@gmail.com>
2023-02-14 20:17:25 +0200

    changed to handling both states with 1 function and made isUnofficial a function

--- lib/views/mobile/team_and_match_selection.dart
+++ lib/views/mobile/team_and_match_selection.dart
@@ -8,11 +8,12 @@ import "package:scouting_frontend/views/common/teams_search_box.dart";
 
 class TeamAndMatchSelection extends StatefulWidget {
   const TeamAndMatchSelection({
-    required this.onTeamChange,
-    required this.onMatchChange,
+    required this.onChange,
   });
-  final void Function(ScheduleMatch) onMatchChange;
-  final void Function(LightTeam?) onTeamChange;
+  final void Function(
+    ScheduleMatch,
+    LightTeam?,
+  ) onChange;
   @override
   State<TeamAndMatchSelection> createState() => TeamAndMatchSelectionState();
 }
@@ -22,11 +23,16 @@ class TeamAndMatchSelectionState extends State<TeamAndMatchSelection> {
   final TextEditingController teamNumberController = TextEditingController();
   ScheduleMatch? scheduleMatch;
   List<LightTeam> teams = <LightTeam>[];
-  bool isUnofficial = false;
+  LightTeam? team;
+  late final Map<String, int> provider =
+      IdProvider.of(context).matchType.nameToId;
+  bool isUnofficial(final ScheduleMatch match) => <int>[
+        provider["Practice"]!,
+        provider["Pre scouting"]!,
+      ].contains(match.matchTypeId);
 
   @override
   Widget build(final BuildContext context) {
-    final Map<String, int> provider = IdProvider.of(context).matchType.nameToId;
     return Column(
       children: <Widget>[
         if (MatchesProvider.of(context).matches.isEmpty)
@@ -37,20 +43,15 @@ class TeamAndMatchSelectionState extends State<TeamAndMatchSelection> {
             matches: MatchesProvider.of(context).matches,
             onChange: (final ScheduleMatch selectedMatch) {
               setState(() {
-                widget.onMatchChange(selectedMatch);
                 scheduleMatch = selectedMatch;
-                widget.onTeamChange(null);
-                teamNumberController.clear();
-                isUnofficial = <int>[
-                  provider["Practice"]!,
-                  provider["Pre scouting"]!
-                ].contains(selectedMatch.matchTypeId);
-                teams = isUnofficial
+                teams = isUnofficial(selectedMatch)
                     ? TeamProvider.of(context).teams
                     : <LightTeam>[
                         ...selectedMatch.blueAlliance,
                         ...selectedMatch.redAlliance
                       ];
+                teamNumberController.clear();
+                widget.onChange(selectedMatch, null);
               });
             },
           ),
@@ -59,14 +60,15 @@ class TeamAndMatchSelectionState extends State<TeamAndMatchSelection> {
         ),
         if (scheduleMatch != null)
           TeamsSearchBox(
-            buildSuggestion: (final LightTeam currentTeam) => isUnofficial
-                ? "${currentTeam.number} ${currentTeam.name}"
-                : scheduleMatch!.getTeamStation(currentTeam) ?? "",
+            buildSuggestion: (final LightTeam currentTeam) =>
+                isUnofficial(scheduleMatch!)
+                    ? "${currentTeam.number} ${currentTeam.name}"
+                    : scheduleMatch!.getTeamStation(currentTeam) ?? "",
             teams: teams,
             typeAheadController: teamNumberController,
             onChange: (final LightTeam team) {
               setState(() {
-                widget.onTeamChange(team);
+                widget.onChange(scheduleMatch!, team);
               });
             },
           ),
