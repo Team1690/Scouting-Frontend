720a183ddf51759c882f50d5c8f14df9d66da5d8
amitcha06 <amitchayko@gmail.com>
2023-02-15 21:21:41 +0200

    fixed lintings that got away

--- lib/views/mobile/screens/coach_team_info_data.dart
+++ lib/views/mobile/screens/coach_team_info_data.dart
@@ -3,8 +3,6 @@ import "package:scouting_frontend/models/map_nullable.dart";
 import "package:scouting_frontend/models/team_model.dart";
 import "package:scouting_frontend/views/common/card.dart";
 import "package:scouting_frontend/views/common/carousel_with_indicator.dart";
-import "package:scouting_frontend/views/constants.dart";
-import "package:scouting_frontend/views/common/dashboard_linechart.dart";
 import "package:scouting_frontend/views/pc/team_info/models/fetch_team_info.dart";
 import "package:scouting_frontend/views/pc/team_info/models/team_info_classes.dart";
 import "package:scouting_frontend/views/pc/team_info/widgets/gamechart/balance_line_chart.dart";
