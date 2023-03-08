357f2c9e16aed5504df25611d30e89609c8886a7
itay <itaynaumann@gmail.com>
2023-03-08 20:35:29 +0200

    adding cycle model

---
+++ lib/models/cycle_model.dart
@@ -0,0 +1,10 @@
+class Cycle {
+  Cycle({
+    required this.endTime,
+    required this.startingTime,
+    required this.length,
+  });
+  final int startingTime;
+  final int endTime;
+  final int length;
+}
