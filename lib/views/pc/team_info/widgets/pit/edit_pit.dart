a2261c22d96a4f02722a5331c4cc5cae62207bb3
amitcha06 <amitchayko@gmail.com>
2023-04-01 21:31:52 +0300

    added edit-pit option to mobile team info

---
+++ lib/views/pc/team_info/widgets/pit/edit_pit.dart
@@ -0,0 +1,47 @@
+import "package:flutter/material.dart";
+import "package:scouting_frontend/models/id_providers.dart";
+import "package:scouting_frontend/views/mobile/pit_vars.dart";
+import "package:scouting_frontend/views/mobile/screens/pit_view/pit_view.dart";
+import "package:scouting_frontend/views/pc/team_info/models/team_info_classes.dart";
+
+class EditPit extends StatefulWidget {
+  const EditPit(
+    this.initialVars,
+  );
+  final PitData initialVars;
+  @override
+  State<EditPit> createState() => _EditPitState();
+}
+
+class _EditPitState extends State<EditPit> {
+  late PitVars vars = fromPitData(widget.initialVars);
+
+  PitVars fromPitData(
+    final PitData? pit,
+  ) {
+    final PitVars vars = PitVars();
+    if (pit == null) {
+      return vars;
+    }
+    vars.canScoreTop = pit.canScoreTop;
+    vars.driveMotorAmount = pit.driveMotorAmount;
+    vars.driveMotorType =
+        IdProvider.of(context).drivemotor.nameToId[pit.driveMotorType];
+    vars.driveTrainType =
+        IdProvider.of(context).driveTrain.nameToId[pit.driveTrainType];
+    vars.driveWheelType = pit.driveWheelType;
+    vars.gearboxPurchased = pit.gearboxPurchased;
+    vars.hasGroundIntake = pit.hasGroundIntake;
+    vars.hasShifter = pit.hasShifer;
+    vars.length = pit.length.toString();
+    vars.notes = pit.notes;
+    vars.spaceBetweenWheels = pit.spaceBetweenWheels.toString();
+    vars.teamId = pit.team.id;
+    vars.weight = pit.weight.toString();
+    vars.width = pit.width.toString();
+    return vars;
+  }
+
+  @override
+  Widget build(final BuildContext context) => PitView(vars);
+}
