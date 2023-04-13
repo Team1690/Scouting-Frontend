436798cd88fc7f2012dd4fb6d5fb82f84e9b771e
amitcha06 <amitchayko@gmail.com>
2023-04-13 13:17:08 +0300

    seperated buttons on mobile

--- lib/views/pc/rating_picklist/rating_pick_list_card.dart
+++ lib/views/pc/rating_picklist/rating_pick_list_card.dart
@@ -42,102 +42,160 @@ class _RatingPicklistCardState extends State<RatingPicklistCard> {
               horizontal: 10,
               vertical: 5,
             ),
-            child: SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: <Widget>[
-                  ToggleButtons(
+            child: Column(
+              children: <Widget>[
+                if (!isPC(context)) ...<Widget>[
+                  Row(
                     children: <Widget>[
-                      const Text("Defense"),
-                      const Text("Drive"),
-                      const Text("Feeder"),
-                      const Text("Ground")
-                    ]
-                        .map(
-                          (final Widget text) => Padding(
-                            padding: EdgeInsets.symmetric(
-                              horizontal: isPC(context) ? 30 : 5,
-                            ),
-                            child: text,
-                          ),
-                        )
-                        .toList(),
-                    isSelected: <bool>[
-                      currentRatingPickList == CurrentRatingPickList.defense,
-                      currentRatingPickList == CurrentRatingPickList.drive,
-                      currentRatingPickList == CurrentRatingPickList.feeder,
-                      currentRatingPickList == CurrentRatingPickList.ground,
-                    ],
-                    onPressed: (final int pressedIndex) {
-                      if (pressedIndex == 0) {
-                        setState(() {
-                          currentRatingPickList = CurrentRatingPickList.defense;
-                          CurrentRatingPickList.defense;
-                        });
-                      } else if (pressedIndex == 1) {
-                        setState(() {
-                          currentRatingPickList = CurrentRatingPickList.drive;
-                          CurrentRatingPickList.drive;
-                        });
-                      } else if (pressedIndex == 2) {
-                        setState(() {
-                          currentRatingPickList = CurrentRatingPickList.feeder;
-                          CurrentRatingPickList.feeder;
-                        });
-                      } else if (pressedIndex == 3) {
-                        setState(() {
-                          currentRatingPickList = CurrentRatingPickList.ground;
-                          CurrentRatingPickList.ground;
-                        });
-                      }
-                    },
-                  ),
-                  IconButton(
-                    onPressed: () =>
-                        save(List<RatingPickListTeam>.from(data), context),
-                    icon: const Icon(Icons.save),
-                  ),
-                  IconButton(
-                    tooltip: "Sort taken",
-                    onPressed: () {
-                      setState(() {
-                        final List<RatingPickListTeam> teamsUntaken = data
-                            .where(
+                      IconButton(
+                        onPressed: () =>
+                            save(List<RatingPickListTeam>.from(data), context),
+                        icon: const Icon(Icons.save),
+                      ),
+                      IconButton(
+                        tooltip: "Sort taken",
+                        onPressed: () {
+                          setState(() {
+                            final List<RatingPickListTeam> teamsUntaken = data
+                                .where(
+                                  (final RatingPickListTeam element) =>
+                                      !element.taken,
+                                )
+                                .toList();
+                            final Iterable<RatingPickListTeam> teamsTaken =
+                                data.where(
                               (final RatingPickListTeam element) =>
-                                  !element.taken,
-                            )
-                            .toList();
-                        final Iterable<RatingPickListTeam> teamsTaken =
-                            data.where(
-                          (final RatingPickListTeam element) => element.taken,
-                        );
-                        data = teamsUntaken..addAll(teamsTaken);
-                        for (int i = 0; i < data.length; i++) {
-                          currentRatingPickList.setIndex(data[i], i);
-                        }
-                      });
-                    },
-                    icon: const Icon(Icons.sort),
+                                  element.taken,
+                            );
+                            data = teamsUntaken..addAll(teamsTaken);
+                            for (int i = 0; i < data.length; i++) {
+                              currentRatingPickList.setIndex(data[i], i);
+                            }
+                          });
+                        },
+                        icon: const Icon(Icons.sort),
+                      ),
+                      IconButton(
+                        tooltip: "Sort By Rating",
+                        onPressed: () {
+                          setState(() {
+                            data.sort(
+                              (
+                                final RatingPickListTeam a,
+                                final RatingPickListTeam b,
+                              ) =>
+                                  currentRatingPickList.getRating(a).compareTo(
+                                        currentRatingPickList.getRating(b),
+                                      ),
+                            );
+                          });
+                        },
+                        icon: const Icon(Icons.star_rate),
+                      )
+                    ],
                   ),
-                  IconButton(
-                    tooltip: "Sort By Rating",
-                    onPressed: () {
-                      setState(() {
-                        data.sort(
-                          (
-                            final RatingPickListTeam a,
-                            final RatingPickListTeam b,
-                          ) =>
-                              currentRatingPickList.getRating(a).compareTo(
-                                    currentRatingPickList.getRating(b),
-                                  ),
-                        );
-                      });
-                    },
-                    icon: const Icon(Icons.star_rate),
-                  )
                 ],
-              ),
+                Row(
+                  children: <Widget>[
+                    ToggleButtons(
+                      children: <Widget>[
+                        const Text("Defense"),
+                        const Text("Drive"),
+                        const Text("Feeder"),
+                        const Text("Ground")
+                      ]
+                          .map(
+                            (final Widget text) => Padding(
+                              padding: EdgeInsets.symmetric(
+                                horizontal: isPC(context) ? 30 : 5,
+                              ),
+                              child: text,
+                            ),
+                          )
+                          .toList(),
+                      isSelected: <bool>[
+                        currentRatingPickList == CurrentRatingPickList.defense,
+                        currentRatingPickList == CurrentRatingPickList.drive,
+                        currentRatingPickList == CurrentRatingPickList.feeder,
+                        currentRatingPickList == CurrentRatingPickList.ground,
+                      ],
+                      onPressed: (final int pressedIndex) {
+                        if (pressedIndex == 0) {
+                          setState(() {
+                            currentRatingPickList =
+                                CurrentRatingPickList.defense;
+                            CurrentRatingPickList.defense;
+                          });
+                        } else if (pressedIndex == 1) {
+                          setState(() {
+                            currentRatingPickList = CurrentRatingPickList.drive;
+                            CurrentRatingPickList.drive;
+                          });
+                        } else if (pressedIndex == 2) {
+                          setState(() {
+                            currentRatingPickList =
+                                CurrentRatingPickList.feeder;
+                            CurrentRatingPickList.feeder;
+                          });
+                        } else if (pressedIndex == 3) {
+                          setState(() {
+                            currentRatingPickList =
+                                CurrentRatingPickList.ground;
+                            CurrentRatingPickList.ground;
+                          });
+                        }
+                      },
+                    ),
+                    if (isPC(context)) ...<Widget>[
+                      IconButton(
+                        onPressed: () =>
+                            save(List<RatingPickListTeam>.from(data), context),
+                        icon: const Icon(Icons.save),
+                      ),
+                      IconButton(
+                        tooltip: "Sort taken",
+                        onPressed: () {
+                          setState(() {
+                            final List<RatingPickListTeam> teamsUntaken = data
+                                .where(
+                                  (final RatingPickListTeam element) =>
+                                      !element.taken,
+                                )
+                                .toList();
+                            final Iterable<RatingPickListTeam> teamsTaken =
+                                data.where(
+                              (final RatingPickListTeam element) =>
+                                  element.taken,
+                            );
+                            data = teamsUntaken..addAll(teamsTaken);
+                            for (int i = 0; i < data.length; i++) {
+                              currentRatingPickList.setIndex(data[i], i);
+                            }
+                          });
+                        },
+                        icon: const Icon(Icons.sort),
+                      ),
+                      IconButton(
+                        tooltip: "Sort By Rating",
+                        onPressed: () {
+                          setState(() {
+                            data.sort(
+                              (
+                                final RatingPickListTeam a,
+                                final RatingPickListTeam b,
+                              ) =>
+                                  currentRatingPickList.getRating(a).compareTo(
+                                        currentRatingPickList.getRating(b),
+                                      ),
+                            );
+                          });
+                        },
+                        icon: const Icon(Icons.star_rate),
+                      )
+                    ]
+                  ],
+                ),
+              ],
             ),
           ),
         ],
