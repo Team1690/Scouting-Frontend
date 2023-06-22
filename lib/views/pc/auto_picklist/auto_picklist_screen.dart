14de80b736e4e422e5721a3950a3cf69f7cafcbb
amitcha06 <amitchayko@gmail.com>
2023-06-22 20:31:14 +0300

    enabled mobile usage of the widget

--- lib/views/pc/auto_picklist/auto_picklist_screen.dart
+++ lib/views/pc/auto_picklist/auto_picklist_screen.dart
@@ -7,6 +7,7 @@ import "package:scouting_frontend/views/constants.dart";
 import "package:scouting_frontend/views/mobile/counter.dart";
 import "package:scouting_frontend/views/mobile/section_divider.dart";
 import "package:scouting_frontend/views/mobile/selector.dart";
+import "package:scouting_frontend/views/mobile/side_nav_bar.dart";
 import "package:scouting_frontend/views/pc/auto_picklist/auto_picklist_widget.dart";
 import "package:scouting_frontend/views/pc/auto_picklist/value_sliders.dart";
 import "package:scouting_frontend/views/pc/picklist/fetch_picklist.dart";
@@ -32,199 +33,210 @@ class _AutoPickListScreenState extends State<AutoPickListScreen> {
   List<AutoPickListTeam> localList = <AutoPickListTeam>[];
 
   @override
-  Widget build(final BuildContext context) => DashboardScaffold(
-        body: Container(
-          margin: const EdgeInsets.symmetric(
-            horizontal: 20,
-            vertical: 10,
+  Widget build(final BuildContext context) => isPC(context)
+      ? DashboardScaffold(
+          body: pickList(context),
+        )
+      : Scaffold(
+          appBar: AppBar(
+            title: const Text("Picklist"),
+            centerTitle: true,
           ),
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                ValueSliders(
-                  onButtonPress: (
-                    final double slider0,
-                    final double slider1,
-                    final double slider2,
-                    final bool swerve,
-                    final bool taken,
-                  ) =>
-                      setState(() {
-                    hasValues = true;
-                    gamepiecesPointsValue = slider0;
-                    gamepiecesSumValue = slider1;
-                    autoBalancePointsValue = slider2;
-                    filterSwerve = swerve;
-                    filterTaken = taken;
-                  }),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                SectionDivider(label: "Actions"),
-                const SizedBox(
-                  height: 10,
-                ),
-                Selector<String>(
-                  options: Picklists.values
-                      .map((final Picklists e) => e.title)
-                      .toList(),
-                  placeholder: "Save as:",
-                  value: saveAs == null ? null : saveAs!.title,
-                  makeItem: (final String picklist) => picklist,
-                  onChange: (final String newTitle) => setState(() {
-                    saveAs = Picklists.values.firstWhere(
-                      (final Picklists picklists) =>
-                          picklists.title == newTitle,
-                    );
-                  }),
-                  validate: (final String unused) => null,
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                RoundedIconButton(
-                  color: Colors.blue,
-                  onPress: () => save(
-                    saveAs,
-                    List<PickListTeam>.from(
-                      localList.map(
-                        (final AutoPickListTeam autoTeam) =>
-                            autoTeam.picklistTeam,
-                      ),
+          drawer: SideNavBar(),
+          body: pickList(context),
+        );
+
+  Container pickList(final BuildContext context) => Container(
+        margin: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 10,
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ValueSliders(
+                onButtonPress: (
+                  final double slider0,
+                  final double slider1,
+                  final double slider2,
+                  final bool swerve,
+                  final bool taken,
+                ) =>
+                    setState(() {
+                  hasValues = true;
+                  gamepiecesPointsValue = slider0;
+                  gamepiecesSumValue = slider1;
+                  autoBalancePointsValue = slider2;
+                  filterSwerve = swerve;
+                  filterTaken = taken;
+                }),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              SectionDivider(label: "Actions"),
+              const SizedBox(
+                height: 10,
+              ),
+              Selector<String>(
+                options: Picklists.values
+                    .map((final Picklists e) => e.title)
+                    .toList(),
+                placeholder: "Save as:",
+                value: saveAs == null ? null : saveAs!.title,
+                makeItem: (final String picklist) => picklist,
+                onChange: (final String newTitle) => setState(() {
+                  saveAs = Picklists.values.firstWhere(
+                    (final Picklists picklists) => picklists.title == newTitle,
+                  );
+                }),
+                validate: (final String unused) => null,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              RoundedIconButton(
+                color: Colors.blue,
+                onPress: () => save(
+                  saveAs,
+                  List<PickListTeam>.from(
+                    localList.map(
+                      (final AutoPickListTeam autoTeam) =>
+                          autoTeam.picklistTeam,
                     ),
-                    context,
                   ),
-                  icon: Icons.save_as,
-                  onLongPress: () {},
+                  context,
                 ),
-                const SizedBox(
-                  height: 10,
-                ),
-                hasValues
-                    ? Padding(
-                        padding: const EdgeInsets.all(defaultPadding),
-                        child: StreamBuilder<List<PickListTeam>>(
-                          stream: fetchPicklist(),
-                          builder: (
-                            final BuildContext context,
-                            final AsyncSnapshot<List<PickListTeam>> snapshot,
-                          ) {
-                            if (snapshot.hasError) {
-                              return Text(snapshot.error.toString());
-                            }
-                            if (snapshot.connectionState ==
-                                ConnectionState.waiting) {
-                              return const Center(
-                                child: CircularProgressIndicator(),
-                              );
-                            }
-                            if (snapshot.data == null) {
-                              return const Center(
-                                child: Text("No Teams"),
-                              );
-                            }
-                            final List<AutoPickListTeam> teamsList = snapshot
-                                .data!
-                                .map(
-                                  (final PickListTeam e) => AutoPickListTeam(
-                                    gamepiecePointsValue: (e
-                                                .avgGamepiecePoints.isNaN
-                                            ? 0
-                                            : e.avgGamepiecePoints) /
-                                        (snapshot.data!
-                                            .map(
-                                              (final PickListTeam e) =>
-                                                  (e.avgGamepiecePoints.isNaN
-                                                      ? 0
-                                                      : e.avgGamepiecePoints),
-                                            )
-                                            .max),
-                                    gamepieceSumValue: (e.avgGamepieces.isNaN
-                                            ? 0
-                                            : e.avgGamepieces) /
-                                        (snapshot.data!
-                                            .map(
-                                              (final PickListTeam e) =>
-                                                  (e.avgGamepieces.isNaN
-                                                      ? 0
-                                                      : e.avgGamepieces),
-                                            )
-                                            .max),
-                                    autoBalancePointsValue: (e
-                                                .avgAutoBalancePoints.isNaN
-                                            ? 0
-                                            : e.avgAutoBalancePoints) /
-                                        (snapshot.data!
-                                            .map(
-                                              (final PickListTeam e) =>
-                                                  (e.avgAutoBalancePoints.isNaN
-                                                      ? 0
-                                                      : e.avgAutoBalancePoints),
-                                            )
-                                            .max),
-                                    picklistTeam: e,
-                                  ),
+                icon: Icons.save_as,
+                onLongPress: () {},
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              hasValues
+                  ? Padding(
+                      padding: const EdgeInsets.all(defaultPadding),
+                      child: StreamBuilder<List<PickListTeam>>(
+                        stream: fetchPicklist(),
+                        builder: (
+                          final BuildContext context,
+                          final AsyncSnapshot<List<PickListTeam>> snapshot,
+                        ) {
+                          if (snapshot.hasError) {
+                            return Text(snapshot.error.toString());
+                          }
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return const Center(
+                              child: CircularProgressIndicator(),
+                            );
+                          }
+                          if (snapshot.data == null) {
+                            return const Center(
+                              child: Text("No Teams"),
+                            );
+                          }
+                          final List<AutoPickListTeam> teamsList =
+                              snapshot.data!
+                                  .map(
+                                    (final PickListTeam e) => AutoPickListTeam(
+                                      gamepiecePointsValue: (e
+                                                  .avgGamepiecePoints.isNaN
+                                              ? 0
+                                              : e.avgGamepiecePoints) /
+                                          (snapshot.data!
+                                              .map(
+                                                (final PickListTeam e) =>
+                                                    (e.avgGamepiecePoints.isNaN
+                                                        ? 0
+                                                        : e.avgGamepiecePoints),
+                                              )
+                                              .max),
+                                      gamepieceSumValue: (e.avgGamepieces.isNaN
+                                              ? 0
+                                              : e.avgGamepieces) /
+                                          (snapshot.data!
+                                              .map(
+                                                (final PickListTeam e) =>
+                                                    (e.avgGamepieces.isNaN
+                                                        ? 0
+                                                        : e.avgGamepieces),
+                                              )
+                                              .max),
+                                      autoBalancePointsValue:
+                                          (e.avgAutoBalancePoints.isNaN
+                                                  ? 0
+                                                  : e.avgAutoBalancePoints) /
+                                              (snapshot.data!
+                                                  .map(
+                                                    (final PickListTeam e) => (e
+                                                            .avgAutoBalancePoints
+                                                            .isNaN
+                                                        ? 0
+                                                        : e.avgAutoBalancePoints),
+                                                  )
+                                                  .max),
+                                      picklistTeam: e,
+                                    ),
+                                  )
+                                  .toList();
+                          teamsList.sort(
+                            (
+                              final AutoPickListTeam a,
+                              final AutoPickListTeam b,
+                            ) =>
+                                (b.autoBalancePointsValue *
+                                            autoBalancePointsValue +
+                                        b.gamepiecePointsValue *
+                                            gamepiecesPointsValue +
+                                        b.gamepieceSumValue *
+                                            gamepiecesSumValue)
+                                    .compareTo(
+                              a.autoBalancePointsValue *
+                                      autoBalancePointsValue +
+                                  a.gamepiecePointsValue *
+                                      gamepiecesPointsValue +
+                                  a.gamepieceSumValue * gamepiecesSumValue,
+                            ),
+                          );
+                          if (filterSwerve) {
+                            localList = teamsList
+                                .where(
+                                  (final AutoPickListTeam element) =>
+                                      element.picklistTeam.drivetrain !=
+                                      "Swerve",
                                 )
                                 .toList();
-                            teamsList.sort(
-                              (
-                                final AutoPickListTeam a,
-                                final AutoPickListTeam b,
-                              ) =>
-                                  (b.autoBalancePointsValue *
-                                              autoBalancePointsValue +
-                                          b.gamepiecePointsValue *
-                                              gamepiecesPointsValue +
-                                          b.gamepieceSumValue *
-                                              gamepiecesSumValue)
-                                      .compareTo(
-                                a.autoBalancePointsValue *
-                                        autoBalancePointsValue +
-                                    a.gamepiecePointsValue *
-                                        gamepiecesPointsValue +
-                                    a.gamepieceSumValue * gamepiecesSumValue,
-                              ),
+                            teamsList.removeWhere(
+                              (final AutoPickListTeam element) =>
+                                  element.picklistTeam.drivetrain != "Swerve",
                             );
-                            if (filterSwerve) {
-                              localList = teamsList
-                                  .where(
-                                    (final AutoPickListTeam element) =>
-                                        element.picklistTeam.drivetrain !=
-                                        "Swerve",
-                                  )
-                                  .toList();
-                              teamsList.removeWhere(
-                                (final AutoPickListTeam element) =>
-                                    element.picklistTeam.drivetrain != "Swerve",
-                              );
-                              localList.insertAll(0, teamsList);
-                              teamsList.clear();
-                              teamsList.addAll(localList);
-                            }
-                            if (filterTaken) {
-                              localList = teamsList
-                                  .where(
-                                    (final AutoPickListTeam element) =>
-                                        element.picklistTeam.taken,
-                                  )
-                                  .toList();
-                              teamsList.removeWhere(
-                                (final AutoPickListTeam element) =>
-                                    element.picklistTeam.taken,
-                              );
-                              localList.insertAll(0, teamsList);
-                            } else {
-                              localList = teamsList;
-                            }
-                            return AutoPickList(uiList: localList);
-                          },
-                        ),
-                      )
-                    : Container()
-              ],
-            ),
+                            localList.insertAll(0, teamsList);
+                            teamsList.clear();
+                            teamsList.addAll(localList);
+                          }
+                          if (filterTaken) {
+                            localList = teamsList
+                                .where(
+                                  (final AutoPickListTeam element) =>
+                                      element.picklistTeam.taken,
+                                )
+                                .toList();
+                            teamsList.removeWhere(
+                              (final AutoPickListTeam element) =>
+                                  element.picklistTeam.taken,
+                            );
+                            localList.insertAll(0, teamsList);
+                          } else {
+                            localList = teamsList;
+                          }
+                          return AutoPickList(uiList: localList);
+                        },
+                      ),
+                    )
+                  : Container()
+            ],
           ),
         ),
       );
